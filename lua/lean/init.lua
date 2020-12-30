14f34f5e6b370350487f977e60fc3805859f4f5b
Julian Berman <Julian@GrayVines.com>
2020-12-30 15:31:46 -0500

    Pull in translations from vscode-lean.

--- lua/lean/init.lua
+++ lua/lean/init.lua
@@ -1,7 +1,28 @@
-local lean = {}
+local lean = {snippets = {}}
 
 function lean.setup(opts)
-  print(vim.inspect(opts))
+  opts = opts or {}
+
+  local snippets = opts.snippets or {}
+  if snippets.enable ~= false then lean.snippets.enable() end
+end
+
+function lean.snippets.enable()
+  local this_file = debug.getinfo(2, "S").source:sub(2)
+  local base_directory = vim.fn.fnamemodify(this_file, ":h:h:h")
+  local translations = base_directory .. '/vscode-lean/translations.json'
+
+  local lean_snippets = {}
+
+  for from, to in pairs(vim.fn.json_decode(vim.fn.readfile(translations))) do
+    lean_snippets["\\" .. from] = to
+  end
+
+  local snippets = require('snippets')
+  local all_snippets = snippets.snippets or {}
+
+  all_snippets.lean = lean_snippets
+  snippets.snippets = all_snippets
 end
 
 return lean
