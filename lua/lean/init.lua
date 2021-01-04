e896b63cfdce25980a7f007cf55466026fdc4262
Julian Berman <Julian@GrayVines.com>
2021-01-04 17:06:59 -0500

    Auto-run LSP by default.

--- lua/lean/init.lua
+++ lua/lean/init.lua
@@ -1,10 +1,17 @@
-local lean = {snippets = {}}
+local lean = {lsp = {}, snippets = {}}
 
 function lean.setup(opts)
   opts = opts or {}
 
   local snippets = opts.snippets or {}
   if snippets.enable ~= false then lean.snippets.enable() end
+
+  local lsp = opts.lsp or {}
+  if lsp.enable ~= false then lean.lsp.enable(lsp) end
+end
+
+function lean.lsp.enable(opts)
+  require('lspconfig').leanls.setup(opts)
 end
 
 function lean.snippets.enable()
