87545b6a00c4b5a5591a3718fabdf10301584367
Julian Berman <Julian@GrayVines.com>
2021-01-16 09:42:26 -0500

    Add a pending test for norcalli/snippets.nvim#17

--- lua/tests/unicode_spec.lua
+++ lua/tests/unicode_spec.lua
@@ -2,19 +2,30 @@ local helpers = require('tests.helpers')
 local feed, insert = helpers.feed, helpers.insert
 
 describe('translations', function()
-  it('expands \\-prefixed predefined substitutions on tab', function()
-    vim.fn.nvim_buf_set_option(0, 'filetype', 'lean')
+  vim.fn.nvim_buf_set_option(0, 'filetype', 'lean')
 
+  it('expands \\-prefixed predefined substitutions on tab', function()
     insert('\\a<Tab>')
     assert.is.equal('α', vim.fn.nvim_get_current_line())
 
-    feed('dd')  -- FIXME: Start with clear buffers...
+    feed('ggdG')  -- FIXME: Start with clear buffers...
   end)
 
   it('does not autoexpand', function()
     insert('\\a')
     assert.is.equal('\\a', vim.fn.nvim_get_current_line())
 
-    feed('dd')  -- FIXME: Start with clear buffers...
+    feed('ggdG')  -- FIXME: Start with clear buffers...
+  end)
+
+  it('expands mid-word', function()
+    pending('norcalli/snippets.nvim#17')
+
+    insert('(\\a<Tab>')
+    --[[ XXX: See nvim-lua/plenary.nvim#49
+    assert.is.equal('(α', vim.fn.nvim_get_current_line())
+    ]]
+
+    feed('ggdG')  -- FIXME: Start with clear buffers...
   end)
 end)
