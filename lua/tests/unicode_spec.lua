c8c334610b36182ebbc9e6aec85a151ed49e9aac
Julian Berman <Julian@GrayVines.com>
2021-01-16 10:56:47 -0500

    Nicer way to make sure we have clear buffers.
    Will work until we have a nicer nicer way (i.e. less
    boilerplate).

--- lua/tests/unicode_spec.lua
+++ lua/tests/unicode_spec.lua
@@ -1,30 +1,29 @@
-local helpers = require('tests.helpers')
-local feed, insert = helpers.feed, helpers.insert
+local insert = require('tests.helpers').insert
 
 describe('translations', function()
   vim.fn.nvim_buf_set_option(0, 'filetype', 'lean')
 
   it('expands \\-prefixed predefined substitutions on tab', function()
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, {}) -- FIXME: setup
+
     insert('\\a<Tab>')
     assert.is.equal('α', vim.fn.nvim_get_current_line())
-
-    feed('ggdG')  -- FIXME: Start with clear buffers...
   end)
 
   it('does not autoexpand', function()
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, {}) -- FIXME: setup
+
     insert('\\a')
     assert.is.equal('\\a', vim.fn.nvim_get_current_line())
-
-    feed('ggdG')  -- FIXME: Start with clear buffers...
   end)
 
   it('expands mid-word', function()
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, {}) -- FIXME: setup
+
     pending('norcalli/snippets.nvim#17', function()
 
       insert('(\\a<Tab>')
       assert.is.equal('(α', vim.fn.nvim_get_current_line())
-
-      feed('ggdG')  -- FIXME: Start with clear buffers...
     end)
   end)
 end)
