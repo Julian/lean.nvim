cc8a459a5e4560783d593a5bd5fb7b8c4c42e53a
Julian Berman <Julian@GrayVines.com>
2021-02-20 12:12:42 -0500

    Initial compe-based implementation of abbreviations.
    Still some kinks, not sure why the slash isn't removed.
    
    But... it does things.
    
    I need to figure out how to test multiple plugin configurations...

--- lua/lean/abbreviations.lua
+++ lua/lean/abbreviations.lua
@@ -12,10 +12,15 @@ function abbreviations.load()
   return lean_abbreviations
 end
 
-local function snippets_nvim_enable(lean_abbreviations)
-  local has_snippets, snippets = pcall(require, 'snippets')
-  if not has_snippets then return end
+local function compe_nvim_enable(compe, lean_abbreviations)
+  local Source = require'lean._compe'.new(lean_abbreviations)
+  compe.register_source('lean_abbreviations', Source)
 
+  local Config = require('compe.config').get()
+  Config.source['lean_abbreviations'] = { disabled = false }
+end
+
+local function snippets_nvim_enable(snippets, lean_abbreviations)
   local all_snippets = snippets.snippets or {}
   all_snippets.lean = lean_abbreviations
   snippets.snippets = all_snippets
@@ -28,7 +33,11 @@ function abbreviations.enable(opts)
     abbreviations["\\" .. from] = to
   end
 
-  snippets_nvim_enable(lean_abbreviations)
+  local has_snippets, snippets = pcall(require, 'snippets')
+  if has_snippets then snippets_nvim_enable(snippets, lean_abbreviations) end
+
+  local has_compe, compe = pcall(require, 'compe')
+  if has_compe then compe_nvim_enable(compe, lean_abbreviations) end
 end
 
 return abbreviations
