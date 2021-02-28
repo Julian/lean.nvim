aba9008add648a1f13731efdea225b251a3b7b43
Julian Berman <Julian@GrayVines.com>
2021-02-28 09:57:52 -0500

    Some initial queries for tree-sitter-lean

---
+++ queries/lean/highlights.scm
@@ -0,0 +1,55 @@
+; Variables
+(identifier) @variable
+
+;; Definitions
+
+(def
+  name: (identifier) @function)
+
+(apply
+  name: (identifier) @function)
+
+(element_of
+  type: (identifier) @type
+  field: (identifier) @method)
+
+["if" "then" "else"] @conditional
+
+; Tokens
+
+[
+  "-"
+  ":="
+  "*"
+  "="
+  ">"
+  "=>"
+  "←"
+  "→"
+  "$"
+] @operator
+
+[
+  "catch"
+  "def"
+  "do"
+  "else"
+  "finally"
+  "fun"
+  "let"
+  "return"
+  "structure"
+  "try"
+  "where"
+] @keyword
+
+[(true) (false)] @boolean
+
+(number) @number
+
+(comment) @comment
+(string) @string
+; Reset highlighing in string interpolation
+(interpolation) @none
+;; Error
+(ERROR) @error
