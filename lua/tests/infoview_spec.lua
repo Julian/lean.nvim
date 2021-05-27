ad5dd352e494c7db1f602268d378f6717c173204
Julian Berman <Julian@GrayVines.com>
2021-05-27 18:54:57 -0400

    Whoops, we care about cursor, not position.

--- lua/tests/infoview_spec.lua
+++ lua/tests/infoview_spec.lua
@@ -3,7 +3,7 @@ local clean_buffer = require('tests.helpers').clean_buffer
 describe('infoview', function()
   it('starts with the window position at the top', clean_buffer('',
     function(context)
-      local position = vim.api.nvim_win_get_position(context.infoview.winnr)
-      assert.is.same(0, position[1])
+      local cursor = vim.api.nvim_win_get_cursor(context.infoview.window)
+      assert.is.same(1, cursor[1])
     end))
 end)
