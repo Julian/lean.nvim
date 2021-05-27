ad01033e479aa02658a935df1d5cb58789070c59
Julian Berman <Julian@GrayVines.com>
2021-05-27 09:19:23 -0400

    Turn off swap for all buffers during test runs.
    This generally doesn't affect CI, but locally running the test suite was
    dirtying the host machine, and ultimately I noticed because test runs
    started hanging entirely due to a (hidden) "Too many swap files" error
    message because locally I had 1000 stale swap files from test runs.
    
    (What was here originally was wrong anyhow since it only affected the
    first buffer. I tried doing `vim.api.nvim_buf_set_option('swapfile',
    false)` in the buffer helper to fix this but for some reason that still
    did not prevent these from being created... no idea why not).

--- scripts/minimal_init.lua
+++ scripts/minimal_init.lua
@@ -1,5 +1,5 @@
 vim.o.display = 'lastline'  -- Avoid neovim/neovim#11362
-vim.bo.swapfile = false
+vim.o.directory = ''
 
 local __file__ = debug.getinfo(1).source:match("@(.*)$")
 local lean_nvim_dir = vim.fn.fnamemodify(__file__, ':p:h:h')
