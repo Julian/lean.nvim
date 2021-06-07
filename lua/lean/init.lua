0e5d62156bf3069e43cf8614e7b634ed6df50dec
Julian Berman <Julian@GrayVines.com>
2021-06-07 14:01:22 +0200

    Support buffer-local suggested mapping application.
    Also makes this look a bit more like how e.g. telescope.nvim
    does this, but not completely. I was following
    https://github.com/norcalli/neovim-plugin but there are even better
    ways to do this at this point.
    
    This commit still doesn't actually make `mappings = true` do the
    per-buffer mapping rather than quashing potential global mappings,
    (which is a bug found by @rish987), so will circle back to do that
    afterwards, likely based on Rish's code and an `autocmd`.

--- lua/lean/init.lua
+++ lua/lean/init.lua
@@ -1,11 +1,21 @@
 local lean = {
   lsp = require('lean.lsp'),
   abbreviations = require('lean.abbreviations'),
+
+  mappings = {
+    n = {
+      ["<LocalLeader>i"] = "<Cmd>lua require('lean.infoview').toggle()<CR>";
+      ["<LocalLeader>s"] = "<Cmd>lua require('lean.sorry').fill()<CR>";
+      ["<LocalLeader>t"] = "<Cmd>lua require('lean.trythis').swap()<CR>";
+      ["<LocalLeader>3"] = "<Cmd>lua require('lean.lean3').init()<CR>";
+    };
+    i = {
+    };
+  }
 }
 
 function lean.setup(opts)
   opts = opts or {}
-
 
   local abbreviations = opts.abbreviations or {}
   if abbreviations.enable ~= false then lean.abbreviations.enable(abbreviations) end
@@ -43,10 +53,23 @@ function lean.setup(opts)
   if lsp.enable ~= false then set_cmds(lsp) lean.lsp.enable(lsp) end
 
   if opts.mappings == true then
-    vim.api.nvim_exec(string.format([[
-      autocmd FileType lean lua require'lean.mappings'.use_suggested_mappings()
-      autocmd FileType lean3 lua require'lean.mappings'.use_suggested_mappings()
-    ]]), false)
+    vim.api.nvim_exec([[
+      autocmd FileType lean lua require'lean'.use_suggested_mappings(true)
+      autocmd FileType lean3 lua require'lean'.use_suggested_mappings(true)
+    ]], false)
+  end
+end
+
+function lean.use_suggested_mappings(buffer_local)
+  local opts = { noremap = true }
+  for mode, mode_mappings in pairs(lean.mappings) do
+    for lhs, rhs in pairs(mode_mappings) do
+      if buffer_local then
+        vim.api.nvim_buf_set_keymap(0, mode, lhs, rhs, opts)
+      else
+        vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
+      end
+    end
   end
 end
 
