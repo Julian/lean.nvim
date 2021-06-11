627d8e20af49e30fcd1fb200f953f6466badd3a9
Julian Berman <Julian@GrayVines.com>
2021-06-11 11:21:50 +0200

    Properly detect leanpkg.toml relative to the lean file, not cwd.
    Closes: #13.
    
    Co-authored-by: Rishikesh Vaishnav <rishhvaishnav@gmail.com>

--- lua/lean/lean3.lua
+++ lua/lean/lean3.lua
@@ -1,8 +1,9 @@
 local M = {}
 
+local find_project_root = require('lspconfig.util').root_pattern('leanpkg.toml')
+
 -- Ideally this obviously would use a TOML parser but yeah choosing to
--- do nasty things and not add the dependency for now. Also there's no
--- project root searching being done, maybe there should be.
+-- do nasty things and not add the dependency for now.
 local _MARKER = '.*lean_version.*\".*:3.*'
 
 function M.init()
@@ -10,10 +11,10 @@ function M.init()
   vim.b.lean3 = true
 end
 
-function M.is_lean3_project(default)
-  default = default or false
-  local exists, result = pcall(vim.fn.readfile, "leanpkg.toml")
-  if not exists then return default end
+function M.is_lean3_project()
+  local project_root = find_project_root(vim.api.nvim_buf_get_name(0))
+  if not project_root then return false end
+  local result = vim.fn.readfile(project_root .. '/leanpkg.toml')
   for _, line in ipairs(result) do
     if line:match(_MARKER) then return true end
   end
