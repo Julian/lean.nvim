22f6ae0ffd14e625d3ec494d1807c84bc0b6ba61
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2021-06-13 14:08:48 -0700

    Fix lean3 filetype detection.

--- lua/tests/ft_spec.lua
+++ lua/tests/ft_spec.lua
@@ -1,9 +1,9 @@
 describe('filetype detection', function()
   require('tests.helpers').setup {}
 
-  it('recognizes a lean file',
+  it('recognizes a lean 3 file',
     function(_)
       vim.api.nvim_command("edit lua/tests/fixtures/example-lean3-project/test.lean")
-      assert.is.same(vim.bo.ft, "lean")
+      assert.is.same(vim.bo.ft, "lean3")
     end)
 end)
