0d8ecbbe7b3af4391b812b0a2c8bd2d227c9297c
Julian Berman <Julian@GrayVines.com>
2021-06-21 19:05:24 +0200

    Trim by exact foo to foo in trythis.swap.
    Also make the implementation via neovim API rather than normal mode.
    
    I had hell before I realized the LSP server was speaking characters
    but nvim_buf_set_text speaks bytes.
    
    See neovim/neovim#14281 for the upstream issue that'd probably help.
    
    Refs: #57

--- lua/lean/trythis.lua
+++ lua/lean/trythis.lua
@@ -1,26 +1,70 @@
 local DiagnosticSeverity = require('vim.lsp.protocol').DiagnosticSeverity
+
 local trythis = {}
 
+
 local function suggestions_from(diagnostic)
+  local suggestions = {}
   local trimmed = diagnostic.message:gsub('^.-Try this:%s*', '')
-  return vim.gsplit(trimmed, 'Try this:')
+  for suggestion in vim.gsplit(trimmed, 'Try this:') do
+    table.insert(
+      suggestions,
+      { replacement = vim.trim(suggestion, '\r\n'), range = diagnostic.range }
+    )
+  end
+  return suggestions
 end
 
 --- Swap the first suggestion from Lean with the word under the cursor.
 --
 --  Doesn't do any robust error checking, or allow rotating for later results
 --  yet.
+--
+--  luacheck: ignore
+--  See https://github.com/leanprover/vscode-lean/blob/8ad0609f560f279512ff792589f06d18aa92fb3f/src/tacticsuggestions.ts#L76
+--  for the VSCode implementation.
 function trythis.swap()
   local diagnostics = vim.lsp.diagnostic.get_line_diagnostics(
     0, nil, { severity = DiagnosticSeverity.Information }
   )
   for _, diagnostic in ipairs(diagnostics) do
-    -- luacheck: ignore
-    for each in suggestions_from(diagnostic) do
-      local command = "normal! ciw" .. vim.trim(each)
-      vim.api.nvim_command(command)
+    local suggestions = suggestions_from(diagnostic)
+    if not vim.tbl_isempty(suggestions) then
+      local suggestion = suggestions[1]
+
+      local start_row = suggestion.range.start.line
+      local start_col = vim.fn.byteidx(
+        vim.api.nvim_get_current_line(),
+        suggestion.range.start.character
+      )
+
+      vim.api.nvim_win_set_cursor(0, {start_row + 1, start_col})
+      local end_row, end_col = unpack(vim.fn.searchpos('\\>', 'c'))
+
+      vim.api.nvim_buf_set_text(
+        0,
+        start_row,
+        start_col,
+        end_row - 1,
+        end_col - 1,
+        vim.split(suggestion.replacement, '\n')
+      )
+
+      trythis.trim_unnecessary_mode_switching()
       return
     end
+  end
+end
+
+--- Trim unnecessary switching between tactic and term modes.
+--
+--  Right now only handles `by exact`.
+function trythis.trim_unnecessary_mode_switching()
+  local line = vim.api.nvim_get_current_line()
+  local start_col, end_col = line:find(' by exact ')
+  if start_col ~= nil then
+    local start_row, _ = unpack(vim.api.nvim_win_get_cursor(0))
+    vim.api.nvim_buf_set_text(0, start_row - 1, start_col, start_row - 1, end_col, {})
   end
 end
 
--- lua/tests/trythis_spec.lua
+++ lua/tests/trythis_spec.lua
@@ -15,22 +15,43 @@ example : ∃ n, n = 2 := by whatshouldIdo]], function()
     )
   end))
 
-  -- Emitted by e.g. pretty_cases
-  it('replaces multiline try this messages', helpers.clean_buffer("lean3", [[
-meta def whatshouldIdo := (do tactic.trace "Try this: existsi 2,\nrefl,\n")
-example : ∃ n, n = 2 := by {
-  whatshouldIdo
-}]], function()
-    vim.api.nvim_command('normal 3gg$')
+  it('replaces a single try this from by', helpers.clean_buffer("lean3", [[
+meta def whatshouldIdo := (do tactic.trace "Try this: existsi 2; refl\n")
+example : ∃ n, n = 2 := by whatshouldIdo]], function()
+    vim.api.nvim_command('normal G$bb')
     helpers.wait_for_line_diagnostics()
 
     require('lean.trythis').swap()
-    assert.contents.are([[
-meta def whatshouldIdo := (do tactic.trace "Try this: existsi 2,\nrefl,\n")
-example : ∃ n, n = 2 := by {
-  existsi 2,
-  refl,
-}]])
+    assert.is.same(
+      'example : ∃ n, n = 2 := by existsi 2; refl',
+      vim.api.nvim_get_current_line()
+    )
+  end))
+
+  it('replaces a single try this from earlier in the line', helpers.clean_buffer("lean3", [[
+meta def whatshouldIdo := (do tactic.trace "Try this: existsi 2; refl\n")
+example : ∃ n, n = 2 := by whatshouldIdo]], function()
+    vim.api.nvim_command('normal G0')
+    helpers.wait_for_line_diagnostics()
+
+    require('lean.trythis').swap()
+    assert.is.same(
+      'example : ∃ n, n = 2 := by existsi 2; refl',
+      vim.api.nvim_get_current_line()
+    )
+  end))
+
+  it('replaces a try this with even more unicode', helpers.clean_buffer("lean3", [[
+meta def whatshouldIdo := (do tactic.trace "Try this: existsi 0; intro m; refl")
+example : ∃ n : nat, ∀ m : nat, m = m := by whatshouldIdo]], function()
+    vim.api.nvim_command('normal G$')
+    helpers.wait_for_line_diagnostics()
+
+    require('lean.trythis').swap()
+    assert.is.same(
+      'example : ∃ n : nat, ∀ m : nat, m = m := by existsi 0; intro m; refl',
+      vim.api.nvim_get_current_line()
+    )
   end))
 
   -- Emitted by e.g. hint
@@ -44,6 +65,51 @@ example : ∃ n, n = 2 := by whatshouldIdo]], function()
     require('lean.trythis').swap()
     assert.is.same(
       'example : ∃ n, n = 2 := by finish',
+      vim.api.nvim_get_current_line()
+    )
+  end))
+
+  -- Emitted by e.g. pretty_cases
+  it('replaces multiline try this messages', helpers.clean_buffer("lean3", [[
+meta def whatshouldIdo := (do tactic.trace "Try this: existsi 2,\n  refl,\n")
+example : ∃ n, n = 2 := by {
+  whatshouldIdo
+}]], function()
+    vim.api.nvim_command('normal 3gg$')
+    helpers.wait_for_line_diagnostics()
+
+    require('lean.trythis').swap()
+    assert.contents.are([[
+meta def whatshouldIdo := (do tactic.trace "Try this: existsi 2,\n  refl,\n")
+example : ∃ n, n = 2 := by {
+  existsi 2,
+  refl,
+}]])
+  end))
+
+  -- Emitted by e.g. library_search
+  it('trims by exact foo to just foo', helpers.clean_buffer("lean3", [[
+meta def whatshouldIdo := (do tactic.trace "Try this: exact rfl")
+example {n : nat} : n = n := by whatshouldIdo]], function()
+    vim.api.nvim_command('normal G$')
+    helpers.wait_for_line_diagnostics()
+
+    require('lean.trythis').swap()
+    assert.is.same(
+      'example {n : nat} : n = n := rfl',
+      vim.api.nvim_get_current_line()
+    )
+  end))
+
+  it('replaces squashed suggestions from earlier in the line', helpers.clean_buffer("lean3", [[
+meta def whatshouldIdo := (do tactic.trace "Try this: exact rfl")
+example {n : nat} : n = n := by whatshouldIdo]], function()
+    vim.api.nvim_command('normal G0')
+    helpers.wait_for_line_diagnostics()
+
+    require('lean.trythis').swap()
+    assert.is.same(
+      'example {n : nat} : n = n := rfl',
       vim.api.nvim_get_current_line()
     )
   end))
