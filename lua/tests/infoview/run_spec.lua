90c3aa5e23d4a03d5bb78515968a7e28d5d9ed6f
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2021-06-22 16:30:18 -0700

    Add manual infoview close tests.

--- lua/tests/infoview/run_spec.lua
+++ lua/tests/infoview/run_spec.lua
@@ -9,6 +9,7 @@ require('tests.helpers').setup {
 describe('infoview', function()
   vim.api.nvim_command("edit lua/tests/fixtures/example-lean3-project/test.lean")
 
+  local win = vim.api.nvim_get_current_win()
   local infoview_info = infoview.open()
 
   it('closes',
@@ -18,6 +19,7 @@ describe('infoview', function()
     infoview.close()
     assert.is_false(vim.api.nvim_win_is_valid(infoview_info.window))
     assert.is.equal(num_wins - 1, get_num_wins())
+    assert.is_falsy(infoview.is_open())
   end)
 
   it('remains closed on BufWinEnter',
@@ -34,6 +36,7 @@ describe('infoview', function()
     infoview_info = infoview.open()
     assert.is_true(vim.api.nvim_win_is_valid(infoview_info.window))
     assert.is.equal(num_wins + 1, get_num_wins())
+    assert.is_truthy(infoview.is_open())
   end)
 
   it('remains open on BufWinEnter',
@@ -43,5 +46,28 @@ describe('infoview', function()
     vim.api.nvim_command("edit lua/tests/fixtures/example-lean3-project/test/test1.lean")
     assert.is_true(vim.api.nvim_win_is_valid(infoview_info.window))
     assert.is.equal(num_wins, get_num_wins())
+  end)
+
+  it('manual quit succeeds and updates internal state',
+  function(_)
+    local num_wins = get_num_wins()
+    vim.api.nvim_set_current_win(infoview_info.window)
+    vim.api.nvim_command("quit")
+    assert.is_false(vim.api.nvim_win_is_valid(infoview_info.window))
+    assert.is.equal(num_wins - 1, get_num_wins())
+    assert.is.equal(win, vim.api.nvim_get_current_win())
+    assert.is_falsy(infoview.is_open())
+  end)
+
+  it('manual close succeeds and updates internal state',
+  function(_)
+    infoview_info = infoview.open()
+    local num_wins = get_num_wins()
+    vim.api.nvim_set_current_win(infoview_info.window)
+    vim.api.nvim_command("close")
+    assert.is_false(vim.api.nvim_win_is_valid(infoview_info.window))
+    assert.is.equal(num_wins - 1, get_num_wins())
+    assert.is.equal(win, vim.api.nvim_get_current_win())
+    assert.is_falsy(infoview.is_open())
   end)
 end)
