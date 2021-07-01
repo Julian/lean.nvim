b858370e6314d7d614ec9966ffba206fc5f5d8e3
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2021-07-01 16:28:01 -0700

    Simplify/fix OOP refactor.

--- lua/lean/infoview.lua
+++ lua/lean/infoview.lua
@@ -23,10 +23,7 @@ local _DEFAULT_WIN_OPTIONS = {
 local _NOTHING_TO_SHOW = { "No info found." }
 
 --- An individual infoview.
-local Infoview = { is_open = true }
-
---- An infoview that has been closed.
-local ClosedInfoview = { is_open = false }
+local Infoview = {}
 
 --- Get the ID of the infoview corresponding to the current window.
 local function get_id()
@@ -35,24 +32,36 @@ end
 
 --- Get the infoview corresponding to the current window.
 function infoview.get_current_infoview()
-  return infoview._by_id[get_id()] or ClosedInfoview:new()
+  return infoview._by_id[get_id()]
 end
 
 --- Create a new infoview.
 ---@param id number: the new ID associated with the infoview
 ---@param width number: the width of the new infoview
-function Infoview:new(id, width)
-  width = width or options.width
+---@param open boolean: whether to open the infoview after initializing
+function Infoview:new(id, width, open)
+  local new_infoview = {id = id, width = width}
+  self.__index = self
+  setmetatable(new_infoview, self)
+
+  if not open then new_infoview:close() else new_infoview:open() end
+
+  return new_infoview
+end
+
+--- Open this infoview if it isn't already open
+function Infoview:open()
+  if self.is_open then return vim.deepcopy(self) end
 
   local bufnr = vim.api.nvim_create_buf(false, true)
-  vim.api.nvim_buf_set_name(bufnr, "lean://infoview/" .. id)
+  vim.api.nvim_buf_set_name(bufnr, "lean://infoview/" .. self.id)
   for name, value in pairs(_DEFAULT_BUF_OPTIONS) do
     vim.api.nvim_buf_set_option(bufnr, name, value)
   end
 
   local window_before_split = vim.api.nvim_get_current_win()
 
-  vim.cmd("botright " .. width .. "vsplit")
+  vim.cmd("botright " .. self.width .. "vsplit")
   vim.cmd(string.format("buffer %d", bufnr))
   local window = vim.api.nvim_get_current_win()
   for name, value in pairs(_DEFAULT_WIN_OPTIONS) do
@@ -61,42 +70,51 @@ function Infoview:new(id, width)
   -- Make sure we notice even if someone manually :q's the infoview window.
   set_augroup("LeanInfoviewClose", string.format([[
     autocmd WinClosed <buffer> lua require'lean.infoview'.__was_closed(%d)
-  ]], id))
-
-  local obj = { bufnr = bufnr, window = window }
-  setmetatable(obj, self)
-  self.__index = self
+  ]], self.id))
 
   vim.api.nvim_set_current_win(window_before_split)
 
-  obj.focus_on_current_buffer()
+  self.bufnr = bufnr
+  self.window = window
+  self.is_open = true
 
-  return obj
-end
+  self:focus_on_current_buffer()
 
---- Do nothing, we're already open.
-function Infoview:open()
-  return self
+  return vim.deepcopy(self)
 end
 
 --- Close this infoview.
-function Infoview:close()
-  vim.api.nvim_win_close(self.window, true)
-  set_augroup("LeanInfoviewUpdate", "", true)
+function Infoview:close(win_already_closed)
+  if not self.is_open then
+    -- in case it is nil
+    self.is_open = false
+    return
+  end
+
+  if not win_already_closed then vim.api.nvim_win_close(self.window, true) end
+  self.bufnr = nil
+  self.window = nil
+  self.is_open = false
+
+  self:focus_on_current_buffer()
 end
 
 --- Toggle this infoview being open.
 function Infoview:toggle()
-  self:close()
+  if self.is_open then self:close() else self:open() end
 end
 
 --- Set the currently active Lean buffer to update the infoview.
-function Infoview.focus_on_current_buffer()
+function Infoview:focus_on_current_buffer()
   if not is_lean_buffer() then return end
-  set_augroup("LeanInfoviewUpdate", [[
-    autocmd CursorHold <buffer> lua require'lean.infoview'.__update()
-    autocmd CursorHoldI <buffer> lua require'lean.infoview'.__update()
-  ]], true)
+  if self.is_open then
+    set_augroup("LeanInfoviewUpdate", [[
+      autocmd CursorHold <buffer> lua require'lean.infoview'.__update()
+      autocmd CursorHoldI <buffer> lua require'lean.infoview'.__update()
+    ]], true)
+  else
+    set_augroup("LeanInfoviewUpdate", "", true)
+  end
 end
 
 --- Update this infoview's contents given the new set of lines.
@@ -116,6 +134,7 @@ end
 --- Update the infoview contents appropriately for Lean 4 or 3.
 --- Normally will be called on each CursorHold for a buffer containing Lean.
 function infoview.__update()
+  local id = get_id()
   local update = vim.b.lean3 and lean3.update_infoview or function(set_lines)
     return leanlsp.plain_goal(0, function(_, _, goal)
       leanlsp.plain_term_goal(0, function(_, _, term_goal)
@@ -128,11 +147,13 @@ function infoview.__update()
     end)
   end
 
-  update(function(lines) infoview.get_current_infoview():update(lines) end)
+  update(function(lines) infoview._by_id[id]:update(lines) end)
 end
 
 --- Retrieve the contents of the infoview as a table.
 function Infoview:get_lines(start_line, end_line)
+  if not self.is_open then return end
+
   start_line = start_line or 0
   end_line = end_line or -1
   return vim.api.nvim_buf_get_lines(self.bufnr, start_line, end_line, true)
@@ -140,11 +161,15 @@ end
 
 --- Retrieve the current combined contents of the infoview as a string.
 function Infoview:get_contents()
+  if not self.is_open then return end
+
   return table.concat(self:get_lines(), "\n")
 end
 
 --- Is the infoview not showing anything?
 function Infoview:is_empty()
+  if not self.is_open then return end
+
   return vim.deep_equal(self:get_lines(), _NOTHING_TO_SHOW)
 end
 
@@ -155,38 +180,10 @@ function infoview.close_all()
   end
 end
 
-function ClosedInfoview:new()
-  local obj = {}
-  setmetatable(obj, self)
-  self.__index = self
-  return obj
-end
-
---- Open an infoview.
-function ClosedInfoview.open(_)
-  local id = get_id()
-  local obj = Infoview:new(id)
-  infoview._by_id[id] = obj
-  return obj
-end
-
---- (Re-)open the infoview.
-function ClosedInfoview:toggle()
-  return self:open()
-end
-
---- Unhook the updating callback, since this window is closed.
---- Likely this should really be done by having all buffers share the same
---- augroup for one infoview window (and then clearing them all at once on
---- `close()`).
-function ClosedInfoview.focus_on_current_buffer()
-  set_augroup("LeanInfoviewUpdate", "", true)
-end
-
 --- An infoview was closed, either directly via `Infoview.close` or manually.
 --- Will be triggered via a `WinClosed` autocmd.
 function infoview.__was_closed(id)
-  infoview._by_id[id] = ClosedInfoview:new()
+  infoview._by_id[id]:close(true)
 end
 
 --- Enable and open the infoview across all Lean buffers.
@@ -196,28 +193,29 @@ function infoview.enable(opts)
     autocmd FileType lean3 lua require'lean.infoview'.make_buffer_focusable()
     autocmd FileType lean lua require'lean.infoview'.make_buffer_focusable()
   ]])
-  infoview.set_autoopen(options.autoopen)
 end
 
 --- Configure the infoview to update when this buffer is active.
 function infoview.make_buffer_focusable()
+  -- WinEnter is necessary for the edge case where you have
+  -- a file open in a tab with an infoview and move to a
+  -- new window in a new tab with that same file but no infoview
   set_augroup("LeanInfoviewSetFocus", [[
-    autocmd BufEnter,WinEnter <buffer> lua require'lean.infoview'.get_current_infoview():focus_on_current_buffer()
+    autocmd BufEnter <buffer> lua require'lean.infoview'.maybe_autoopen()
+    autocmd BufEnter,WinEnter <buffer> lua if require'lean.infoview'.get_current_infoview()]] ..
+    [[ then require'lean.infoview'.get_current_infoview():focus_on_current_buffer() end
   ]], true)
 end
 
 --- Set whether a new infoview is automatically opened when entering Lean buffers.
 function infoview.set_autoopen(autoopen)
-  set_augroup("LeanInfoviewAutoopen",
-    autoopen and "autocmd BufEnter * lua require'lean.infoview'.maybe_autoopen()" or ""
-  )
+  options.autoopen = autoopen
 end
 
 --- Open an infoview for the current buffer if it isn't already open.
 function infoview.maybe_autoopen()
-  if not is_lean_buffer() then return end
   local id = get_id()
-  if not infoview._by_id[id] then infoview._by_id[id] = Infoview:new(id) end
+  if not infoview._by_id[id] then infoview._by_id[id] = Infoview:new(id, options.width, options.autoopen) end
 end
 
 return infoview
