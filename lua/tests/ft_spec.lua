9b2c2994425b4dd4fc55f38be1ce37ffd81feecb
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2021-07-06 09:15:01 -0700

    Add test cases for #81.

--- lua/tests/ft_spec.lua
+++ lua/tests/ft_spec.lua
@@ -3,12 +3,24 @@ describe('filetype detection', function()
   it('recognizes a lean 3 file',
     function(_)
       vim.api.nvim_command("edit lua/tests/fixtures/example-lean3-project/test.lean")
-      assert.is.same(vim.bo.ft, "lean3")
+      assert.is.same("lean3", vim.bo.ft)
+    end)
+
+  it('recognizes a new lean 3 file',
+    function(_)
+      vim.api.nvim_command("edit lua/tests/fixtures/example-lean3-project/foo.lean")
+      assert.is.same("lean3", vim.bo.ft)
     end)
 
   it('recognizes a lean 4 file',
     function(_)
       vim.api.nvim_command("edit lua/tests/fixtures/example-lean4-project/Test.lean")
-      assert.is.same(vim.bo.ft, "lean")
+      assert.is.same("lean", vim.bo.ft)
+    end)
+
+  it('recognizes a new lean 4 file',
+    function(_)
+      vim.api.nvim_command("edit lua/tests/fixtures/example-lean4-project/Foo.lean")
+      assert.is.same("lean", vim.bo.ft)
     end)
 end)
