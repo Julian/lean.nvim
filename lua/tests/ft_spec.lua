b1d128b0cecd2d5ce598cff4f9d14c0d5f1ebe17
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2021-07-06 17:19:48 -0700

    More filetype unit tests.

--- lua/tests/ft_spec.lua
+++ lua/tests/ft_spec.lua
@@ -1,26 +1,61 @@
 require('tests.helpers').setup {}
 describe('filetype detection', function()
-  it('recognizes a lean 3 file',
-    function(_)
-      vim.api.nvim_command("edit lua/tests/fixtures/example-lean3-project/test.lean")
-      assert.is.same("lean3", vim.bo.ft)
+  describe('lean 3', function()
+    describe('existing', function()
+      it('root file',
+        function(_)
+          vim.api.nvim_command("edit lua/tests/fixtures/example-lean3-project/test.lean")
+          assert.is.same("lean3", vim.bo.ft)
+        end)
+
+      it('nested file',
+        function(_)
+          vim.api.nvim_command("edit lua/tests/fixtures/example-lean3-project/test/test1.lean")
+          assert.is.same("lean3", vim.bo.ft)
+        end)
     end)
 
-  it('recognizes a new lean 3 file',
-    function(_)
-      vim.api.nvim_command("edit lua/tests/fixtures/example-lean3-project/foo.lean")
-      assert.is.same("lean3", vim.bo.ft)
+    describe('new', function()
+      it('root file',
+        function(_)
+          vim.api.nvim_command("edit lua/tests/fixtures/example-lean3-project/foo.lean")
+          assert.is.same("lean3", vim.bo.ft)
+        end)
+
+      it('nested file',
+        function(_)
+          vim.api.nvim_command("edit lua/tests/fixtures/example-lean3-project/test/foo.lean")
+          assert.is.same("lean3", vim.bo.ft)
+        end)
+    end)
+  end)
+
+  describe('lean 4', function()
+    describe('existing', function()
+      it('root file',
+        function(_)
+          vim.api.nvim_command("edit lua/tests/fixtures/example-lean4-project/Test.lean")
+          assert.is.same("lean", vim.bo.ft)
+        end)
+
+      it('nested file',
+        function(_)
+          vim.api.nvim_command("edit lua/tests/fixtures/example-lean4-project/Test/Test1.lean")
+          assert.is.same("lean", vim.bo.ft)
+        end)
     end)
 
-  it('recognizes a lean 4 file',
-    function(_)
-      vim.api.nvim_command("edit lua/tests/fixtures/example-lean4-project/Test.lean")
-      assert.is.same("lean", vim.bo.ft)
+    describe('new', function()
+      it('root file',
+        function(_)
+          vim.api.nvim_command("edit lua/tests/fixtures/example-lean4-project/Foo.lean")
+          assert.is.same("lean", vim.bo.ft)
+        end)
+      it('nested file',
+        function(_)
+          vim.api.nvim_command("edit lua/tests/fixtures/example-lean4-project/Test/Foo.lean")
+          assert.is.same("lean", vim.bo.ft)
+        end)
     end)
-
-  it('recognizes a new lean 4 file',
-    function(_)
-      vim.api.nvim_command("edit lua/tests/fixtures/example-lean4-project/Foo.lean")
-      assert.is.same("lean", vim.bo.ft)
-    end)
+  end)
 end)
