257da0acbe60abfc58296a1a0fd1439d2df97487
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2021-07-28 16:21:27 -0700

    Add buffer tracking.

--- lua/tests/infoview/start_spec.lua
+++ lua/tests/infoview/start_spec.lua
@@ -1,49 +1,49 @@
 local infoview = require('lean.infoview')
 local fixtures = require('tests.fixtures')
 
-require('tests.helpers').setup { infoview = { autoopen = true } }
+require('tests.helpers').setup {}
 describe('infoview', function()
   describe("startup", function()
-    local src_win = vim.api.nvim_get_current_win()
-    it('created valid infoview',
+    it('cursor stays in source window on open',
       function(_)
         vim.api.nvim_command('edit ' .. fixtures.lean3_project.some_existing_file)
-        assert.opened_infoview()
+        infoview.get_current_infoview():open()
+        assert.kept_win({infoview.get_current_infoview().window})
+      end)
+
+    it('created valid infoview',
+      function(_)
+        assert.opened_infoview_buf()
       end)
 
     it('starts with the window position at the top',
       function(_)
         local cursor = vim.api.nvim_win_get_cursor(infoview.get_current_infoview().window)
         assert.is.same(1, cursor[1])
-      end)
-
-    it('cursor starts in source window',
-      function(_)
-        assert.is.same(src_win, vim.api.nvim_get_current_win())
       end)
   end)
 
   describe("new tab", function()
-    local src_win
 
-    it('created valid distinct infoview',
+    it('cursor stays in source window on open',
       function(_)
         vim.api.nvim_command("tabnew")
+        assert.created_buf()
         assert.created_win()
-        src_win = vim.api.nvim_get_current_win()
         vim.api.nvim_command('edit ' .. fixtures.lean_project.some_existing_file)
-        assert.opened_infoview()
+        infoview.get_current_infoview():open()
+        assert.kept_win({infoview.get_current_infoview().window})
+      end)
+
+    it('created valid infoview',
+      function(_)
+        assert.opened_infoview_buf()
       end)
 
     it('starts with the window position at the top',
       function(_)
         local cursor = vim.api.nvim_win_get_cursor(infoview.get_current_infoview().window)
         assert.is.same(1, cursor[1])
-      end)
-
-    it('cursor starts in source window',
-      function(_)
-        assert.is.same(src_win, vim.api.nvim_get_current_win())
       end)
   end)
 end)
