dd1a73ac8f9a342e5a6bc54fcdd03e2434e18f72
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2021-08-16 16:02:59 -0700

    Use `lean --version` to infer filetype when `elan` is installed.

--- lua/lean/ft.lua
+++ lua/lean/ft.lua
@@ -1,5 +1,12 @@
-return {
-  detect = function()
-    vim.opt.filetype = require('lean.lean3').__detect() and 'lean3' or 'lean'
+local M = {}
+if vim.fn.executable"elan" then
+  M.detect =
+  vim.schedule_wrap(function()
+    vim.opt.filetype = require('lean.lean3').__detect_elan() and 'lean3' or 'lean'
+  end)
+else
+  M.detect = function()
+    vim.opt.filetype = require('lean.lean3').__detect_regex() and 'lean3' or 'lean'
   end
-}
+end
+return M
