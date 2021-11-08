b8a00c7697cb4b144b41915114cca28d2c6bf55c
Julian Berman <Julian@GrayVines.com>
2021-11-08 12:33:32 -0500

    Port the Lean3 ftplugin to Lua.
    Refs: #98

---
+++ ftplugin/lean3/lean.lua
@@ -0,0 +1,28 @@
+if vim.b.did_ftplugin then return end
+vim.b.did_ftplugin = 1
+
+vim.opt.wildignore:append[[*.olean]]
+
+vim.opt_local.iskeyword = [[@,48-57,_,-,!,#,$,%]]
+vim.opt_local.comments = [[s0:/-,mb:\ ,ex:-/,:--]]
+vim.opt_local.commentstring=[[/-\ %s\ -/]]
+
+vim.opt_local.includeexpr = [[substitute(v:fname, '\.', '/', 'g') . '.lean']]
+
+vim.opt_local.expandtab = true
+vim.opt_local.shiftwidth = 2
+vim.opt_local.softtabstop = 2
+
+vim.opt_local.matchpairs:append[[⟨:⟩]]
+
+-- Matchit support
+if vim.g.loaded_matchit and not vim.b.match_words then
+  vim.b.match_ignorecase = 0
+  vim.b.match_words = table.concat(
+    {
+        [[\<begin\>:\<end\ze\%(\,\|$\)]],
+        [[\<\%(namespace\|section\)\s\+\(.\{-}\)\>:\<end\s\+\1\>]],
+    },
+    ','
+  )
+end
