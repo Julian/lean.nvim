1837371196e20380dfb348f38938c8bc232ef5d7
Julian Berman <Julian@GrayVines.com>
2021-12-13 15:32:18 -0500

    Revert "Disable incremental sync in neovim 0.6.0"
    This reverts commit 3c6eb4326939afaf0eb5f949979e7755882239d0.
    
    See https://github.com/Julian/lean.nvim/pull/219#issuecomment-992759760

--- lua/lean/lsp.lua
+++ lua/lean/lsp.lua
@@ -9,9 +9,6 @@ function lsp.enable(opts)
       vim.lsp.handlers['textDocument/publishDiagnostics'](...)
     end;
   })
-  -- workaround for sync bugs in <=0.6.0
-  -- https://github.com/neovim/neovim/issues/16624
-  opts.flags = vim.tbl_extend('keep', opts.flags or {}, { allow_incremental_sync = false })
   require('lspconfig').leanls.setup(opts)
 end
 
