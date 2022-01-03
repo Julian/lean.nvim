f33f4d03143d0db2d68a14e1bde15e912c83c698
Julian Berman <Julian@GrayVines.com>
2022-01-03 14:25:00 +0000

    Port the final pin tests.
    Landing on 12.5s for the suite run.

--- lua/tests/infoview/pin_spec.lua
+++ lua/tests/infoview/pin_spec.lua
@@ -7,20 +7,20 @@ local helpers = require('tests.helpers')
 
 require('lean').setup{ lsp = { enable = true } }
 
-describe('infoview pins', function()
+describe('infoview pins', helpers.clean_buffer('lean', dedent[[
+  theorem has_tactic_goal : p ∨ q → q ∨ p := by
+    intro h
+    cases h with
+    | inl h1 =>
+      apply Or.inr
+      exact h1
+    | inr h2 =>
+      apply Or.inl
+      assumption
+  ]], function()
   -- FIXME: This test seems to fail in CI on 0.5.1, and only on macOS.
   if vim.version().major >= 1 or vim.version().minor >= 6 then
-    it('can be placed and cleared', helpers.clean_buffer('lean', dedent[[
-      theorem has_tactic_goal : p ∨ q → q ∨ p := by
-        intro h
-        cases h with
-        | inl h1 =>
-          apply Or.inr
-          exact h1
-        | inr h2 =>
-          apply Or.inl
-          assumption
-      ]], function()
+    it('can be placed and cleared', function()
       local filename = vim.api.nvim_buf_get_name(0)
 
       helpers.move_cursor{ to = {7, 5} }
@@ -99,21 +99,11 @@ describe('infoview pins', function()
         h2 : q
         ⊢ q ∨ p
       ]]
-    end))
+    end)
   end
 
   -- FIXME: This seems to fail with errors saying it's misusing vim.schedule.
-  pending('can be re-placed after being cleared', helpers.clean_buffer('lean', dedent[[
-    theorem has_tactic_goal : p ∨ q → q ∨ p := by
-      intro h
-      cases h with
-      | inl h1 =>
-        apply Or.inr
-        exact h1
-      | inr h2 =>
-        apply Or.inl
-        assumption
-    ]], function()
+  pending('can be re-placed after being cleared', function()
     helpers.move_cursor{ to = {4, 5} }
     infoview.get_current_infoview().info:add_pin()
     infoview.get_current_infoview().info:clear_pins()
@@ -133,20 +123,161 @@ describe('infoview pins', function()
       h1 : p
       ⊢ q ∨ p
     ]], vim.api.nvim_buf_get_name(0)))
-  end))
+  end)
 
-  describe('diff pins',  helpers.clean_buffer('lean', dedent[[
-    theorem has_tactic_goal : p ∨ q → q ∨ p := by
-      intro h
-      cases h with
-      | inl h1 =>
-        apply Or.inr
-        exact h1
-      | inr h2 =>
-        apply Or.inl
-        assumption
-  ]], function()
+  describe('edits around pin', function()
 
+    infoview.get_current_infoview().info:clear_pins()
+    helpers.move_cursor{ to = {4, 12} }
+    infoview.get_current_infoview().info:add_pin()
+
+    it('moves pin when lines are added above it', function()
+      vim.api.nvim_buf_set_lines(0, 0, 0, true, { 'theorem foo : 2 = 2 := rfl', '' })
+      helpers.move_cursor{ to = {1, 24} }
+      helpers.wait_for_infoview_contents('expected type.*1 goal')
+      assert.infoview_contents.are(string.format([[
+        ▶ expected type (1:24-1:27)
+        ⊢ 2 = 2
+
+        -- %s at 6:11
+        ▶ 1 goal
+        case inl
+        p q : Prop
+        h1 : p
+        ⊢ q ∨ p
+      ]], vim.api.nvim_buf_get_name(0)))
+    end)
+
+    it('moves pin when lines are removed above it', function()
+      assert.infoview_contents.are(string.format([[
+        ▶ expected type (1:24-1:27)
+        ⊢ 2 = 2
+
+        -- %s at 6:11
+        ▶ 1 goal
+        case inl
+        p q : Prop
+        h1 : p
+        ⊢ q ∨ p
+      ]], vim.api.nvim_buf_get_name(0)))
+
+      helpers.move_cursor{ to = {3, 50} }
+      vim.api.nvim_buf_set_lines(0, 0, 2, true, {})
+
+      helpers.wait_for_infoview_contents('1 goal.*1 goal')
+      assert.infoview_contents.are(string.format([[
+        ▶ 1 goal
+        p q : Prop
+        ⊢ p ∨ q → q ∨ p
+
+        -- %s at 4:11
+        ▶ 1 goal
+        case inl
+        p q : Prop
+        h1 : p
+        ⊢ q ∨ p
+      ]], vim.api.nvim_buf_get_name(0)))
+    end)
+
+    it('does not move pin when lines are added or removed below it', function()
+      assert.infoview_contents.are(string.format([[
+        ▶ 1 goal
+        p q : Prop
+        ⊢ p ∨ q → q ∨ p
+
+        -- %s at 4:11
+        ▶ 1 goal
+        case inl
+        p q : Prop
+        h1 : p
+        ⊢ q ∨ p
+      ]], vim.api.nvim_buf_get_name(0)))
+
+      vim.api.nvim_buf_set_lines(0, -1, -1, true, { '', 'theorem foo : 2 = 2 := rfl' })
+
+      helpers.move_cursor{ to = {11, 24} }
+      helpers.wait_for_infoview_contents('expected type.*1 goal')
+      assert.infoview_contents.are(string.format([[
+        ▶ expected type (11:24-11:27)
+        ⊢ 2 = 2
+
+        -- %s at 4:11
+        ▶ 1 goal
+        case inl
+        p q : Prop
+        h1 : p
+        ⊢ q ∨ p
+      ]], vim.api.nvim_buf_get_name(0)))
+
+      vim.api.nvim_buf_set_lines(0, 9, 11, true, {})
+
+      helpers.move_cursor{ to = {1, 50} }
+      helpers.wait_for_infoview_contents('1 goal.*1 goal')
+      assert.infoview_contents.are(string.format([[
+        ▶ 1 goal
+        p q : Prop
+        ⊢ p ∨ q → q ∨ p
+
+        -- %s at 4:11
+        ▶ 1 goal
+        case inl
+        p q : Prop
+        h1 : p
+        ⊢ q ∨ p
+      ]], vim.api.nvim_buf_get_name(0)))
+    end)
+
+    it('moves pin when changes are made on its line before its column', function()
+      helpers.move_cursor{ to = {4, 7} }
+      vim.cmd[[normal cl37]]  -- h1 -> h37
+      helpers.move_cursor{ to = {1, 50} }
+      helpers.wait_for_infoview_contents('1 goal.*1 goal.*h37')
+      assert.infoview_contents.are(string.format([[
+        ▶ 1 goal
+        p q : Prop
+        ⊢ p ∨ q → q ∨ p
+
+        -- %s at 4:12
+        ▶ 1 goal
+        case inl
+        p q : Prop
+        h37 : p
+        ⊢ q ∨ p
+      ]], vim.api.nvim_buf_get_name(0)))
+    end)
+
+    it('does not move pin when changes are made on its line after its column', function()
+      assert.infoview_contents.are(string.format([[
+        ▶ 1 goal
+        p q : Prop
+        ⊢ p ∨ q → q ∨ p
+
+        -- %s at 4:12
+        ▶ 1 goal
+        case inl
+        p q : Prop
+        h37 : p
+        ⊢ q ∨ p
+      ]], vim.api.nvim_buf_get_name(0)))
+      helpers.move_cursor{ to = {4, 13} }
+      vim.cmd[[normal a    ]]
+      helpers.wait_for_infoview_contents('1 goal.*1 goal.*h37')
+      assert.infoview_contents.are(string.format([[
+        ▶ 1 goal
+        p q : Prop
+        ⊢ p ∨ q → q ∨ p
+
+        -- %s at 4:12
+        ▶ 1 goal
+        case inl
+        p q : Prop
+        h37 : p
+        ⊢ q ∨ p
+      ]], vim.api.nvim_buf_get_name(0)))
+    end)
+  end)
+
+  describe('diff pins',  function()
     local lean_window
 
     it('opens a diff window when placed', function()
@@ -234,11 +365,11 @@ describe('infoview pins', function()
         end
       end
       vim.api.nvim_set_current_win(diff_window)
-      vim.cmd("quit")
+      vim.cmd('quit')
       assert.are.same(
           { lean_window, infoview.get_current_infoview().window },
           vim.api.nvim_tabpage_list_wins(0)
       )
     end)
-  end))
-end)
+  end)
+end))
