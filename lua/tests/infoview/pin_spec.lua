350c7e0795f31ac00aed2d661a5bc3c3e1cf758c
Julian Berman <Julian@GrayVines.com>
2022-01-03 11:45:50 +0000

    Rewrite the diff-pin pin tests.
    Down below 15s.

--- lua/tests/infoview/pin_spec.lua
+++ lua/tests/infoview/pin_spec.lua
@@ -134,4 +134,111 @@ describe('infoview pins', function()
       ⊢ q ∨ p
     ]], vim.api.nvim_buf_get_name(0)))
   end))
+
+  describe('diff pins',  helpers.clean_buffer('lean', dedent[[
+    theorem has_tactic_goal : p ∨ q → q ∨ p := by
+      intro h
+      cases h with
+      | inl h1 =>
+        apply Or.inr
+        exact h1
+      | inr h2 =>
+        apply Or.inl
+        assumption
+  ]], function()
+
+    local lean_window
+
+    it('opens a diff window when placed', function()
+      lean_window = vim.api.nvim_get_current_win()
+      local current_infoview = infoview.get_current_infoview()
+      assert.are.same_elements(
+        { lean_window, current_infoview.window },
+        vim.api.nvim_tabpage_list_wins(0)
+      )
+
+      helpers.move_cursor{ to = {3, 2} }
+      infoview.set_diff_pin()
+
+      local windows = vim.api.nvim_tabpage_list_wins(0)
+      local diff_window
+      for _, window in ipairs(windows) do
+        if window ~= lean_window and window ~= current_infoview.window then
+          diff_window = window
+        end
+      end
+
+      assert.are.same_elements(
+        { lean_window, current_infoview.window, diff_window },
+        windows
+      )
+
+      assert.is_true(vim.api.nvim_win_get_option(current_infoview.window, 'diff'))
+      assert.is_true(vim.api.nvim_win_get_option(diff_window, 'diff'))
+    end)
+
+    it('closes the diff window if the infoview is closed', function()
+      local current_infoview = infoview.get_current_infoview()
+      assert.is.equal(3, #vim.api.nvim_tabpage_list_wins(0))
+      current_infoview:close()
+      assert.are.same({ lean_window }, vim.api.nvim_tabpage_list_wins(0))
+    end)
+
+    it('reopens a diff window when the infoview is reopened', function()
+      assert.are.same({ lean_window }, vim.api.nvim_tabpage_list_wins(0))
+      local current_infoview = infoview.get_current_infoview()
+
+      current_infoview:open()
+
+      local windows = vim.api.nvim_tabpage_list_wins(0)
+      -- The window is not necessarily the same one as before.
+      local diff_window
+      for _, window in ipairs(windows) do
+        if window ~= lean_window and window ~= current_infoview.window then
+          diff_window = window
+        end
+      end
+
+      assert.are.same_elements(
+        { lean_window, current_infoview.window, diff_window },
+        windows
+      )
+
+      assert.is_true(vim.api.nvim_win_get_option(current_infoview.window, 'diff'))
+      assert.is_true(vim.api.nvim_win_get_option(diff_window, 'diff'))
+    end)
+
+    it('closes when cleared', function()
+      assert.is.equal(3, #vim.api.nvim_tabpage_list_wins(0))
+      infoview.clear_diff_pin()
+      assert.are.same(
+          { lean_window, infoview.get_current_infoview().window },
+          vim.api.nvim_tabpage_list_wins(0)
+      )
+    end)
+
+    it('can be re-placed', function()
+      assert.is.equal(2, #vim.api.nvim_tabpage_list_wins(0))
+      helpers.move_cursor{ to = {3, 2} }
+      infoview.set_diff_pin()
+      assert.is.equal(3, #vim.api.nvim_tabpage_list_wins(0))
+    end)
+
+    it('can be :quit', function()
+      assert.is.equal(3, #vim.api.nvim_tabpage_list_wins(0))
+      local current_infoview = infoview.get_current_infoview()
+      local diff_window
+      for _, window in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+        if window ~= lean_window and window ~= current_infoview.window then
+          diff_window = window
+        end
+      end
+      vim.api.nvim_set_current_win(diff_window)
+      vim.cmd("quit")
+      assert.are.same(
+          { lean_window, infoview.get_current_infoview().window },
+          vim.api.nvim_tabpage_list_wins(0)
+      )
+    end)
+  end))
 end)
