b8c3e9c422b93ab2cd8cbaf5b013fa44f46293b1
Julian Berman <Julian@GrayVines.com>
2022-02-13 11:45:33 -0500

    Add a test for simp [foo] without only.
    (This was already correct because the pattern will match the shorter
    part).
    
    Also some minor test simplification.

--- lua/tests/switch_spec.lua
+++ lua/tests/switch_spec.lua
@@ -4,73 +4,72 @@ local clean_buffer = helpers.clean_buffer
 require('lean').setup{}
 
 describe('switch', function()
-  it('switches between left and right', clean_buffer('lean3', [[
-#check mul_right_comm
-]], function()
+  it('switches between left and right',
+    clean_buffer('lean3', [[#check mul_right_comm]], function()
     vim.api.nvim_command('normal! 1gg12|')
     vim.cmd('Switch')
-    assert.is.same('#check mul_left_comm', vim.api.nvim_get_current_line())
+    assert.contents.are[[#check mul_left_comm]]
 
     vim.cmd('Switch')
-    assert.is.same('#check mul_right_comm', vim.api.nvim_get_current_line())
+    assert.contents.are[[#check mul_right_comm]]
   end))
 
-  it('switches between top and bot', clean_buffer('lean3', [[
-#check with_top
-]], function()
+  it('switches between top and bot',
+    clean_buffer('lean3', [[#check with_top]], function()
     vim.api.nvim_command('normal! 1gg$')
     vim.cmd('Switch')
-    assert.is.same('#check with_bot', vim.api.nvim_get_current_line())
+    assert.contents.are[[#check with_bot]]
 
     vim.cmd('Switch')
-    assert.is.same('#check with_top', vim.api.nvim_get_current_line())
+    assert.contents.are[[#check with_top]]
   end))
 
-  it('does not switch between top and bot prefix', clean_buffer('lean3', [[
-#check tops
-]], function()
+  it('does not switch between top and bot prefix',
+    clean_buffer('lean3', [[#check tops]], function()
     vim.api.nvim_command('normal! 1gg$hh')
     vim.cmd('Switch')
-    assert.is.same('#check tops', vim.api.nvim_get_current_line())
+    assert.contents.are[[#check tops]]
   end))
 
-  it('does not switch between top and bot suffix', clean_buffer('lean3', [[
-#check stop
-]], function()
+  it('does not switch between top and bot suffix',
+    clean_buffer('lean3', [[#check stop]], function()
     vim.api.nvim_command('normal! 1gg$')
     vim.cmd('Switch')
-    assert.is.same('#check stop', vim.api.nvim_get_current_line())
+    assert.contents.are[[#check stop]]
   end))
 
-  it('switches between exact <> and refine <>', clean_buffer('lean3', [[
-exact ⟨foo, bar⟩
-]], function()
+  it('switches between exact <> and refine <>',
+    clean_buffer('lean3', [[exact ⟨foo, bar⟩]], function()
     vim.api.nvim_command('normal! 1gg0')
     vim.cmd('Switch')
-    assert.is.same('refine ⟨foo, bar⟩', vim.api.nvim_get_current_line())
+    assert.contents.are[[refine ⟨foo, bar⟩]]
   end))
 
-  it('does not switch between exact foo and refine foo', clean_buffer('lean3', [[
-exact foo
-]], function()
+  it('does not switch between exact foo and refine foo',
+    clean_buffer('lean3', [[exact foo]], function()
     vim.api.nvim_command('normal! 1gg0')
     vim.cmd('Switch')
-    assert.is.same('exact foo', vim.api.nvim_get_current_line())
+    assert.contents.are[[exact foo]]
   end))
 
-  it('switches between simp only [foo] and simp', clean_buffer("lean3", [[
-simp only [foo, bar, baz]
-]], function()
+  it('switches between simp only [foo] and simp',
+    clean_buffer("lean3", [=[simp only [foo, bar, baz]]=], function()
     vim.api.nvim_command('normal! 1gg0')
     vim.cmd('Switch')
-    assert.is.same('simp', vim.api.nvim_get_current_line())
+    assert.contents.are[[simp]]
   end))
 
-  it('switches between simp and squeeze_simp', clean_buffer("lean3", [[
-simp
-]], function()
+  it('switches between simp and squeeze_simp',
+    clean_buffer("lean3", [[simp]], function()
     vim.api.nvim_command('normal! 1gg0')
     vim.cmd('Switch')
-    assert.is.same('squeeze_simp', vim.api.nvim_get_current_line())
+    assert.contents.are[[squeeze_simp]]
+  end))
+
+  it('switches between simp [foo] and squeeze_simp [foo]',
+    clean_buffer("lean3", [=[simp [foo, bar, baz]]=], function()
+    vim.api.nvim_command('normal! 1gg0')
+    vim.cmd('Switch')
+    assert.contents.are[=[squeeze_simp [foo, bar, baz]]=]
   end))
 end)
