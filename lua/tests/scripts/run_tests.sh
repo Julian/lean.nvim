232c89126aca77197184406653b20c0d77710572
Julian Berman <Julian@GrayVines.com>
2022-03-16 10:58:29 -0400

    Remove stray quotes.

--- lua/tests/scripts/run_tests.sh
+++ lua/tests/scripts/run_tests.sh
@@ -6,7 +6,7 @@ if [ -z ${TEST_FILE+x} ]; then
         nvim --headless --noplugin -u scripts/minimal_init.lua -c "PlenaryBustedDirectory lua/tests/ { minimal_init = './scripts/minimal_init.lua'; sequential = true; keep_going = true }"
     fi
 elif [ -d ${TEST_FILE} ]; then
-  nvim --headless --noplugin -u scripts/minimal_init.lua -c "PlenaryBustedDirectory '$TEST_FILE' { minimal_init = './scripts/minimal_init.lua' }"
+  nvim --headless --noplugin -u scripts/minimal_init.lua -c "PlenaryBustedDirectory $TEST_FILE { minimal_init = './scripts/minimal_init.lua' }"
 else
-  nvim --headless --noplugin -u scripts/minimal_init.lua -c "PlenaryBustedFile '$TEST_FILE'"
+  nvim --headless --noplugin -u scripts/minimal_init.lua -c "PlenaryBustedFile $TEST_FILE"
 fi
