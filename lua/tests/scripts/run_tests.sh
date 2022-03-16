5c7a900ff261fa4deccc79a122123af599f5d362
Julian Berman <Julian@GrayVines.com>
2022-03-16 08:15:39 -0400

    Cut test suite time down to 7s.
    They run fine in parallel now.

--- lua/tests/scripts/run_tests.sh
+++ lua/tests/scripts/run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ -z ${TEST_FILE+x} ]; then
-  nvim --headless --noplugin -u scripts/minimal_init.lua -c "PlenaryBustedDirectory lua/tests/ { minimal_init = './scripts/minimal_init.lua'; sequential = true; keep_going = true }"
+  nvim --headless --noplugin -u scripts/minimal_init.lua -c "PlenaryBustedDirectory lua/tests/ { minimal_init = './scripts/minimal_init.lua' }"
 else
   nvim --headless --noplugin -u scripts/minimal_init.lua -c "PlenaryBustedFile $TEST_FILE"
 fi
