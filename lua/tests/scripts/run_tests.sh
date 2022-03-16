ae3f33197f02d5ed639a725f645384e913a058cb
Julian Berman <Julian@GrayVines.com>
2022-03-16 08:17:37 -0400

    Support running just a single test directory

--- lua/tests/scripts/run_tests.sh
+++ lua/tests/scripts/run_tests.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 if [ -z ${TEST_FILE+x} ]; then
   nvim --headless --noplugin -u scripts/minimal_init.lua -c "PlenaryBustedDirectory lua/tests/ { minimal_init = './scripts/minimal_init.lua' }"
+elif [ -d ${TEST_FILE} ]; then
+  nvim --headless --noplugin -u scripts/minimal_init.lua -c "PlenaryBustedDirectory $TEST_FILE { minimal_init = './scripts/minimal_init.lua' }"
 else
   nvim --headless --noplugin -u scripts/minimal_init.lua -c "PlenaryBustedFile $TEST_FILE"
 fi
