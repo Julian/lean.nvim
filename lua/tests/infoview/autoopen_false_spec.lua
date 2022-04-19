88bc80d521f9f28f32a5c5bb7d9519ab448d533b
Julian Berman <Julian@GrayVines.com>
2022-04-19 14:15:49 -0400

    Make sure the autoopen_false tests fail when it's completely broken.
    Obviously this is hacky nonsense, but I don't see why the
    heck it's happening yet.
    
    For now this will satisfy fully closing #245, and if/when we
    figure out why the hack is needed we can delete the line.
    
    Closes: #245.

--- lua/tests/infoview/autoopen_false_spec.lua
+++ lua/tests/infoview/autoopen_false_spec.lua
@@ -11,6 +11,17 @@ describe('infoview', function()
   it('does not automatically open infoviews', function(_)
     assert.is.equal(1, #vim.api.nvim_tabpage_list_wins(0))
     vim.cmd('edit! ' .. fixtures.lean3_project.some_existing_file)
+    -- FIXME: This obviously shouldn't require running twice, but without
+    --        it, somehow the test run differs from interactive use!
+    --        Specifically, ft.detect doesn't run at all until the second
+    --        time a file is opened :/. This must be related to what I've
+    --        observed about `make nvim` -- that if you edit a file, you
+    --        have to open it twice to open the infoview, so when we fix
+    --        that, this should be fixed as well -- but for now, this needs
+    --        to be here twice to prevent regressions like #245.
+    --        To know whether you can remove this, undo the change from #245
+    --        and ensure this test properly fails.
+    vim.cmd('edit! ' .. fixtures.lean3_project.some_existing_file)
     lean_window = vim.api.nvim_get_current_win()
     assert.windows.are(lean_window)
   end)
