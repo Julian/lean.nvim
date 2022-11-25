21bd963d583999d63ffb8f4270e25f4ec993fce8
Julian Berman <Julian@GrayVines.com>
2022-11-25 15:34:11 -0500

    Use the native Lua API for healthchecking.
    (It has slightly different output, so tweak the test).
    
    This is available (and has been) in stable neovim for a
    yearish.

--- lua/lean/health.lua
+++ lua/lean/health.lua
@@ -2,18 +2,15 @@
 --- Support for `:checkhealth` for lean.nvim.
 ---@brief ]]
 
+local health = {}
+
 local Job = require('plenary.job')
 local subprocess_check_output = require('lean._util').subprocess_check_output
 
-local health = {
-  report = { __index = function (_, key) return vim.fn['health#report_' .. key] end }
-}
-setmetatable(health.report, health.report)
-
 local function check_lean_runnable()
   local lean = subprocess_check_output{ command = "lean", args = { "--version" } }
-  health.report.ok('`lean --version`')
-  health.report.info(table.concat(lean, '\n'))
+  vim.health.report_ok('`lean --version`')
+  vim.health.report_info(table.concat(lean, '\n'))
 end
 
 local function check_lean3ls_runnable()
@@ -24,15 +21,15 @@ local function check_lean3ls_runnable()
   })
   if succeeded then
     lean3ls:sync()
-    health.report.ok('`lean-language-server`')
+    vim.health.report_ok('`lean-language-server`')
   else
-    health.report.warn('`lean-language-server` not found, lean 3 support will not work')
+    vim.health.report_warn('`lean-language-server` not found, lean 3 support will not work')
   end
 end
 
 local function check_for_timers()
   if not vim.tbl_isempty(vim.fn.timer_info()) then
-    health.report.warn(
+    vim.health.report_warn(
       'You have active timers, which can degrade infoview (CursorMoved) ' ..
       'performance. See https://github.com/Julian/lean.nvim/issues/92.'
     )
@@ -43,10 +40,10 @@ end
 ---
 --- Call me via `:checkhealth lean`.
 function health.check()
-    health.report.start('lean.nvim')
-    check_lean_runnable()
-    check_lean3ls_runnable()
-    check_for_timers()
+  vim.health.report_start('lean.nvim')
+  check_lean_runnable()
+  check_lean3ls_runnable()
+  check_for_timers()
 end
 
 return health
--- lua/tests/checkhealth_spec.lua
+++ lua/tests/checkhealth_spec.lua
@@ -4,10 +4,10 @@ describe('checkhealth', function()
   it('passes the health check', function()
     vim.api.nvim_command('silent checkhealth lean')
     assert.has_match([[
-## lean.nvim
-  - .*OK:.* `lean ----version`
-  - .*INFO:.* Lean .*version .+
-  - .*OK:.* `lean--language--server`
+.*lean.nvim.*
+.*- .*OK.* `lean ----version`
+.*- Lean .*version .+
+.*- .*OK.* `lean--language--server`
 ]], table.concat(vim.api.nvim_buf_get_lines(0, 0, -1, false), '\n'))
   end)
 end)
