a2bc56f05edf42c7f3b44b0721daf81092096a67
Gihan Marasingha <gihan.marasingha@gmail.com>
2023-09-20 17:02:50 +0100

    perform an appropriate sorry.fill depending on the filetype

--- lua/lean/sorry.lua
+++ lua/lean/sorry.lua
@@ -9,9 +9,11 @@ local function calculate_indent(line)
     indent = vim.fn.indent(vim.fn.prevnonblank(line))
   end
 
-  local line_text = vim.fn.getline(line):gsub("^%s*", "")
-  if line_text:sub(1, 2) == "\194\183" then
-    indent = indent + 2
+  if not vim.bo.filetype == "lean3" then
+    local line_text = vim.fn.getline(line):gsub("^%s*", "")
+    if line_text:sub(1, 2) == "\194\183" then
+      indent = indent + 2
+    end
   end
 
   return string.rep(' ', indent)
@@ -22,15 +24,22 @@ function sorry.fill()
   local params = vim.lsp.util.make_position_params()
   local responses = vim.lsp.buf_request_sync(0, '$/lean/plainGoal', params)
 
+  if vim.bo.filetype == "lean3" then
+      sorrytext = "{ sorry },"
+      offset = 2
+  else
+      sorrytext = "· sorry "
+      offset = 3
+  end
   for _, response in pairs(responses) do
     if not response.result or not response.result.goals or vim.tbl_isempty(response.result.goals) then return end
     local goals = #response.result.goals
     if goals then
       local index = vim.api.nvim_win_get_cursor(0)[1]
       local indent = calculate_indent(index)
-      local lines = tbl_repeat(indent .. "· sorry ", goals)
+      local lines = tbl_repeat(indent .. sorrytext, goals)
       vim.api.nvim_buf_set_lines(0, index, index, true, lines)
-      vim.api.nvim_win_set_cursor(0, { index + 1, #indent + 3 })  -- the 's'
+      vim.api.nvim_win_set_cursor(0, { index + 1, #indent + offset })  -- the 's'
       return
     end
   end
