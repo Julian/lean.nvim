4c0a32c31d74d0d746e1d07f6818981f7049d0f7
Julian Berman <Julian@GrayVines.com>
2023-10-22 12:35:39 -0400

    Whoops, and the lean 3 implementation.

--- lua/tests/lean3/fixtures.lua
+++ lua/tests/lean3/fixtures.lua
@@ -11,4 +11,13 @@ fixtures.project = {
   some_nested_nonexisting_file = root .. '/example-project/src/bar/does_not_exist.lean'
 }
 
+function fixtures.project_files()
+  return pairs{
+    ["existing"] = fixtures.project.some_existing_file,
+    ["nested existing"] = fixtures.project.some_nested_existing_file,
+    ["nonexisting"] = fixtures.project.some_nonexisting_file,
+    ["nested nonexisting"] = fixtures.project.some_nested_nonexisting_file
+  }
+end
+
 return fixtures
