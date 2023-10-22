7bc96bc52ed65954aa9139b5d75b9f1b87c85147
Julian Berman <Julian@GrayVines.com>
2023-10-22 15:32:36 -0400

    Do this properly using luassert.

--- lua/tests/helpers.lua
+++ lua/tests/helpers.lua
@@ -61,14 +61,11 @@ end
 function helpers.move_cursor(opts)
   local window = opts.window or 0
 
-  if not vim.deep_equal(opts.to, vim.api.nvim_win_get_cursor(window)) then
-    local message = util.s[[
-      Cursor is already at %s.
-      If you just want to ensure the cursor is at this location,
-      use nvim_win_set_cursor directly.
-    ]]
-    error(message:format(vim.inspect(opts.to)))
-  end
+  assert.message(util.s[[
+    Cursor is already at %s.
+    If you just want to ensure the cursor is at this location,
+    use nvim_win_set_cursor directly.
+  ]]).are_not.same(opts.to, vim.api.nvim_win_get_cursor(window))
 
   vim.api.nvim_win_set_cursor(window, opts.to)
   vim.cmd.doautocmd('CursorMoved')
