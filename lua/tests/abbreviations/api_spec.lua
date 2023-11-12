a0874b8fca21339ffcfed6fe25c1e9bb65332374
Julian Berman <Julian@GrayVines.com>
2023-11-12 16:07:59 -0500

    Use is_empty in the abbreviations tests, and remove what's already the default.

--- lua/tests/abbreviations/api_spec.lua
+++ lua/tests/abbreviations/api_spec.lua
@@ -1,6 +1,8 @@
-local abbreviations = require('lean.abbreviations')
+require 'tests.helpers'
 
-require('lean').setup{ abbreviations = { enable = true } }
+local abbreviations = require 'lean.abbreviations'
+
+require('lean').setup {}
 
 describe('programmatic API', function()
   it('provides access to loaded abbreviations', function()
@@ -8,26 +10,17 @@ describe('programmatic API', function()
   end)
 
   it('provides reverse-lookup of loaded abbreviations', function()
-    assert.is.same(
-      { [2] = {'\\a', '\\Ga', '\\alpha'}},
-      abbreviations.reverse_lookup('α')
-    )
+    assert.is.same({ [2] = { '\\a', '\\Ga', '\\alpha' } }, abbreviations.reverse_lookup 'α')
   end)
 
   it('allows random trailing junk', function()
-    assert.is.same(
-      {
-        [3] = { '\\^-' },
-        [5] = { '\\-', '\\-1', '\\sy', '\\^-1', '\\inv' },
-      },
-      abbreviations.reverse_lookup('⁻¹something something')
-    )
+    assert.is.same({
+      [3] = { '\\^-' },
+      [5] = { '\\-', '\\-1', '\\sy', '\\^-1', '\\inv' },
+    }, abbreviations.reverse_lookup '⁻¹something something')
   end)
 
   it('returns an empty table for non-existing abbreviations', function()
-    assert.is.same(
-      {},
-      abbreviations.reverse_lookup(' ')
-    )
+    assert.is.empty(abbreviations.reverse_lookup ' ')
   end)
 end)
