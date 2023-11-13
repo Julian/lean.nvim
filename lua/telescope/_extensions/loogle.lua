bf476a18b7c7b000f6aa1e08a59fcf0aae3ea60f
Julian Berman <Julian@GrayVines.com>
2023-11-12 21:36:59 -0500

    And the Loogle plugin.

--- lua/telescope/_extensions/loogle.lua
+++ lua/telescope/_extensions/loogle.lua
@@ -1,8 +1,8 @@
-local telescope = require('telescope');
-local loogle = require('telescope._extensions.loogle.loogle_search');
+local loogle = require 'telescope._extensions.loogle.loogle_search'
+local telescope = require 'telescope'
 
 return telescope.register_extension {
   exports = {
-    loogle = loogle.find
-  }
+    loogle = loogle.find,
+  },
 }
--- lua/telescope/_extensions/loogle/loogle_search.lua
+++ lua/telescope/_extensions/loogle/loogle_search.lua
@@ -1,11 +1,11 @@
-local actions = require('telescope.actions')
-local actions_state = require('telescope.actions.state')
-local config = require('telescope.config')
-local finders = require('telescope.finders')
-local pickers = require('telescope.pickers')
-local previewers = require('telescope.previewers')
+local actions = require 'telescope.actions'
+local actions_state = require 'telescope.actions.state'
+local config = require 'telescope.config'
+local finders = require 'telescope.finders'
+local pickers = require 'telescope.pickers'
+local previewers = require 'telescope.previewers'
 
-local loogle = require('lean.loogle')
+local loogle = require 'lean.loogle'
 
 local M = {}
 
@@ -18,11 +18,12 @@ M.find = function(telescope_opts)
   telescope_opts = vim.tbl_extend('keep', telescope_opts or {}, {})
 
   local type
-  vim.ui.input(
-    { prompt = 'Enter a type pattern to search on Loogle: ' },
-    function(choice) type = choice end
-  )
-  if not type or type == '' then return end
+  vim.ui.input({ prompt = 'Enter a type pattern to search on Loogle: ' }, function(choice)
+    type = choice
+  end)
+  if not type or type == '' then
+    return
+  end
 
   local results, err = loogle.search(type)
   if err then
@@ -33,40 +34,41 @@ M.find = function(telescope_opts)
     return
   end
 
-  pickers.new(telescope_opts, {
-    prompt_title = 'Loogle Search Filter',
-    finder = finders.new_table{
-      results = results,
-      ---@param entry LoogleResult
-      entry_maker = function(entry)
-        return {
-          value = entry,
-          display = entry.name,
-          ordinal = entry.name
-        }
-      end
-    },
-    sorter = config.values.generic_sorter(telescope_opts),
+  pickers
+    .new(telescope_opts, {
+      prompt_title = 'Loogle Search Filter',
+      finder = finders.new_table {
+        results = results,
+        ---@param entry LoogleResult
+        entry_maker = function(entry)
+          return {
+            value = entry,
+            display = entry.name,
+            ordinal = entry.name,
+          }
+        end,
+      },
+      sorter = config.values.generic_sorter(telescope_opts),
 
-    previewer = previewers.new_buffer_previewer{
-      ---@param entry LoogleTelescopeEntry
-      define_preview = function(self, entry)
-        local bufnr = self.state.bufnr
-        local lines = loogle.template(entry.value)
-        require('telescope.previewers.utils').highlighter(bufnr, 'lean')
-        vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
-      end
-    },
-    attach_mappings = function(prompt_bufnr, _)
-      actions.select_default:replace(function()
-        actions.close(prompt_bufnr)
-        ---@type LoogleTelescopeEntry
-        local selection = actions_state.get_selected_entry()
-        vim.api.nvim_put({ selection.value.name }, 'c', true, true)
-      end)
-      return true
-    end,
-  }):find()
-
+      previewer = previewers.new_buffer_previewer {
+        ---@param entry LoogleTelescopeEntry
+        define_preview = function(self, entry)
+          local bufnr = self.state.bufnr
+          local lines = loogle.template(entry.value)
+          require('telescope.previewers.utils').highlighter(bufnr, 'lean')
+          vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
+        end,
+      },
+      attach_mappings = function(prompt_bufnr, _)
+        actions.select_default:replace(function()
+          actions.close(prompt_bufnr)
+          ---@type LoogleTelescopeEntry
+          local selection = actions_state.get_selected_entry()
+          vim.api.nvim_put({ selection.value.name }, 'c', true, true)
+        end)
+        return true
+      end,
+    })
+    :find()
 end
 return M
