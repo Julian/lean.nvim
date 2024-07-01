e7835e3a1ff52a7510040a8d213b406608986693
Julian Berman <Julian@GrayVines.com>
2024-07-01 15:45:20 -0400

    Evade some neovim bug which causes duplicate portrait infoview windows.
    I have no idea what's going on here, but this is the line which causes
    it, and it does not occur for landscape infoviews.
    
    Seems almost certainly to be a bug in winfixbuf.
    
    To reproduce, before this change, have a portrait orientation and open a
    lean file, and you should observe 3 windows opened instead of 2.
    
    winfixbuf seems to be the culprit, and vim.schedule fixes the partial
    initialization state.
    
    Don't ask me anything further on why, that'll be for another day.

--- ftplugin/leaninfo.lua
+++ ftplugin/leaninfo.lua
@@ -10,5 +10,20 @@ vim.wo.winfixheight = true
 vim.wo.winfixwidth = true
 vim.wo.wrap = true
 if vim.fn.exists('&winfixbuf') > 0 then
-  vim.wo.winfixbuf = true
+  local wo = vim.wo[vim.api.nvim_get_current_win()]
+  -- FIXME: This is obviously ridiculous, but there's seemingly some neovim bug
+  --        here which needs minimizing.
+  --        The symptoms are that when opening a stacked (vertical) infoview
+  --        -- and only when opening a vertical one -- we were opening *2*
+  --        identically named windows (lean://.../curr)
+  --        The line which triggers this behavior is the line which sets
+  --        filetype = leaninfo, i.e. which triggers this file to run, and
+  --        specifically the line which triggers it is this one.
+  --        So something is going wrong where winfixbuf cannot run that early
+  --        on in a window being set up, even though we do not change the
+  --        infoview buffer anytime after that filetype line.
+  --        Somehow vim.schedule delays things long enough that they work
+  --        correctly, including winfixbuf getting set.
+  --        We'll minimize. Some day.
+  vim.schedule(function() wo.winfixbuf = true end)
 end
