31e0289c776ef1d967e424a5093433e57a32b295
Julian Berman <Julian@GrayVines.com>
2024-07-04 16:15:19 -0400

    Add a sample codespaces setup using lazyvim.
    This at least lets someone with 0 nvim setup try things out.

---
+++ .devcontainer/lazyvim/lean.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    'Julian/lean.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'neovim/nvim-lspconfig',
+    },
+    opts = {
+      infoview = {
+        horizontal_position = 'top',
+        show_processing = false,
+      },
+      mappings = true
+    },
+  }
+}
