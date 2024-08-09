10040f5953d71ddbc2872d2b88b6f5c84a6e7432
Julian Berman <Julian@GrayVines.com>
2024-08-09 12:35:14 -0400

    Add a hasty version of lean.progress.percentage for completion percentage.

--- lua/lean/progress.lua
+++ lua/lean/progress.lua
@@ -47,4 +47,20 @@ function M.is_processing_at(params)
   return false
 end
 
+---Calculate the percentage of a buffer which finished processing.
+---@param bufnr? number the buffer number, defaulting to 0
+---@return number the percentage of *finished* lines as a number from 0 to 100
+function M.percentage(bufnr)
+  bufnr = bufnr or 0
+  local proc_info = M.proc_infos[vim.uri_from_bufnr(bufnr)]
+  if not proc_info then
+    return 1
+  end
+
+  local finished = vim.iter(proc_info):fold(0, function(acc, range)
+    return acc + range.range['end'].line - range.range.start.line
+  end)
+  return 100 - 100 * finished / vim.api.nvim_buf_line_count(bufnr)
+end
+
 return M
