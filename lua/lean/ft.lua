fd7262270c68667fae85ad4703ac84e34c84d5e1
Julian Berman <Julian@GrayVines.com>
2024-09-09 11:58:29 -0400

    Another vim.iter use.

--- lua/lean/ft.lua
+++ lua/lean/ft.lua
@@ -15,12 +15,9 @@ end
 ---Make the given buffer `nomodifiable` if its file name matches a configured list.
 function ft.__maybe_make_nomodifiable(bufnr)
   local name = vim.api.nvim_buf_get_name(bufnr)
-  for _, pattern in ipairs(options.nomodifiable) do
-    if name:match(pattern) then
-      vim.bo[bufnr].modifiable = false
-      return
-    end
-  end
+  vim.bo[bufnr].modifiable = vim.iter(options.nomodifiable):any(function(pattern)
+    return not name:match(pattern)
+  end)
 end
 
 return ft
