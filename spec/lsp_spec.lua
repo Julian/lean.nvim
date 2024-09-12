fc27098c52ff0d4829aff88b3cce3a474b7e2bc4
Julian Berman <Julian@GrayVines.com>
2024-09-12 12:26:06 -0400

    Add some even more basic LSP attach tests.
    The _uninitialized argument seems to grab LSP clients that aren't
    yet active (which we want, since let's not wait for them to get active
    just to assert we're starting them).

---
+++ spec/lsp_spec.lua
@@ -0,0 +1,30 @@
+---@brief [[
+---Tests for basic (auto-)attaching of LSP clients.
+---@brief ]]
+
+local fixtures = require 'spec.fixtures'
+local helpers = require 'spec.helpers'
+
+require('lean').setup {}
+
+describe('LSP', function()
+  assert.is.empty(vim.lsp.get_clients { bufnr = 0, name = 'leanls', _uninitialized = true })
+
+  it('is attached to .lean files within projects', function()
+    vim.cmd.edit(fixtures.project.some_existing_file)
+    assert.is.same(1, #vim.lsp.get_clients { bufnr = 0, name = 'leanls', _uninitialized = true })
+  end)
+
+  it(
+    'is attached to single .lean files',
+    helpers.clean_buffer(function()
+      assert.is.same(1, #vim.lsp.get_clients { bufnr = 0, name = 'leanls', _uninitialized = true })
+    end)
+  )
+
+  it('is not attached to non-Lean files', function()
+    vim.cmd.split 'some_non_lean_file.tmp'
+    assert.is.empty(vim.lsp.get_clients { bufnr = 0, name = 'leanls', _uninitialized = true })
+    vim.cmd.close { bang = true }
+  end)
+end)
