1b44c095788feac345281a121a82c245dde0af1f
Julian Berman <Julian@GrayVines.com>
2024-10-15 15:07:21 -0400

    Another minor tweak to cleaning up our abbreviations mappings.
    Loop over mappings we set rather than all the ones the user may have set
    in a buffer.
    
    It still does annoy me a bit that vim.keymap.get doesn't exist (which is
    wontfixed upstream for now in neovim/neovim#24333 and which encourages
    still using the vimscript API, so that's what we do).

--- lua/lean/abbreviations.lua
+++ lua/lean/abbreviations.lua
@@ -144,19 +144,11 @@ local function insert_char_pre()
       end
     end)
 
-    for imap in vim.iter(vim.api.nvim_buf_get_keymap(0, 'i')) do
-      local lhs = imap.lhs
-      local rhs = imap.rhs or ''
-      if mappings[lhs] then
-        cleanups[lhs] = function()
-          vim.api.nvim_buf_set_keymap(0, 'i', lhs, rhs, {
-            nowait = imap.nowait,
-            silent = imap.silent,
-            script = imap.script,
-            expr = imap.expr,
-            unique = imap.unique,
-            callback = imap.callback,
-          })
+    for key in vim.iter(mappings) do
+      local current = vim.fn.maparg(key, 'i', false, true)
+      if current.buffer then
+        cleanups[key] = function()
+          vim.fn.mapset(current)
         end
       end
     end
