5ca5e3b1a7bd7e66d88fae8eb7e3c3aae0b47b78
Julian Berman <Julian@GrayVines.com>
2024-10-24 09:34:22 -0400

    Add basic tests for our subprocess running code.

--- spec/util_spec.lua
+++ spec/util_spec.lua
@@ -1,9 +1,9 @@
-local dedent = require('lean._util').dedent
+local util = require 'lean._util'
 
 describe('dedent', function()
   it('dedents multiline strings by their common prefix', function()
     assert.is.equal(
-      dedent [[
+      util.dedent [[
         foo bar
         baz quux
       ]],
@@ -12,17 +12,17 @@ describe('dedent', function()
   end)
 
   it('leaves dedented lines alone', function()
-    assert.is.equal('foo bar', dedent 'foo bar')
+    assert.is.equal('foo bar', util.dedent 'foo bar')
   end)
 
   it('dedents indented single lines', function()
-    assert.is.equal('foo ', dedent ' foo ')
+    assert.is.equal('foo ', util.dedent ' foo ')
   end)
 
   it('ignores empty lines', function()
     assert.is.equal(
       '\nfoo bar\n\n\nbaz quux\n\n',
-      dedent [[
+      util.dedent [[
 
         foo bar
 
@@ -34,6 +34,33 @@ describe('dedent', function()
   end)
 
   it('leaves single lines with trailing whitespace alone', function()
-    assert.is.equal('foo ', dedent 'foo ')
+    assert.is.equal('foo ', util.dedent 'foo ')
+  end)
+end)
+
+describe('subprocesses', function()
+  describe('check_output', function()
+    it('returns subprocess output', function()
+      local stdout = util.subprocess_check_output {
+        command = 'lean',
+        args = { '--run', '--stdin' },
+        writer = util.dedent [[
+          def main : IO Unit := IO.println "Hello, world!"
+        ]],
+      }
+      assert.are.same({ 'Hello, world!' }, stdout)
+    end)
+
+    it('errors for unsuccessful processes', function()
+      local successful, error = pcall(util.subprocess_check_output, {
+        command = 'lean',
+        args = { '--run', '--stdin' },
+        writer = util.dedent [[
+            def main : IO Unit := IO.Process.exit 37
+          ]],
+      })
+      assert.is_false(successful)
+      assert.is.truthy(error:match 'exit status 37')
+    end)
   end)
 end)
