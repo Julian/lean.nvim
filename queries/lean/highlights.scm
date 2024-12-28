76e48e4278756a51b9f5a6f1f636437831c198ce
Julian Berman <Julian@GrayVines.com>
2024-12-28 12:21:02 -0500

    WIP

--- queries/lean/highlights.scm
+++ queries/lean/highlights.scm
@@ -6,67 +6,67 @@
   name: (identifier) @namespace)
 
 (arrow) @type
-(product) @type
-
+; (product) @type
+;
 ;; Declarations
 
 [
-  "abbrev"
+  ; "abbrev"
   "def"
-  "theorem"
-  "constant"
-  "instance"
-  "axiom"
-  "example"
-  "inductive"
-  "structure"
-  "class"
-
-  "deriving"
+  ; "theorem"
+  ; "constant"
+  ; "instance"
+  ; "axiom"
+  ; "example"
+  ; "inductive"
+  ; "structure"
+  ; "class"
+  ;
+  ; "deriving"
 
   "section"
   "namespace"
 ] @keyword
 
-(attributes
-  (identifier) @function)
+; (attributes
+;   (identifier) @function)
 
-(abbrev
-  name: (identifier) @type)
+; (abbrev
+;   name: (identifier) @type)
 (def
   name: (identifier) @function)
-(theorem
-  name: (identifier) @function)
-(constant
-  name: (identifier) @type)
-(instance
-  name: (identifier) @function)
-(instance
-  type: (identifier) @type)
-(axiom
-  name: (identifier) @function)
-(structure
-  name: (identifier) @type)
-(structure
-  extends: (identifier) @type)
-
-(where_decl
-  type: (identifier) @type)
-
+; (theorem
+;   name: (identifier) @function)
+; (constant
+;   name: (identifier) @type)
+; (instance
+;   name: (identifier) @function)
+; (instance
+;   type: (identifier) @type)
+; (axiom
+;   name: (identifier) @function)
+; (structure
+;   name: (identifier) @type)
+; (structure
+;   extends: (identifier) @type)
+;
+; (where_decl
+;   type: (identifier) @type)
+;
 (implicit_binder
     type: (identifier) @type)
 (explicit_binder
     type: (identifier) @type)
-
-(proj
-  name: (identifier) @field)
-
-(binders
-  type: (identifier) @type)
-
+;
+; (proj
+;   name: (identifier) @field)
+;
+; (binders
+;   type: (identifier) @type)
+;
 ["if" "then" "else"] @conditional
-
-["for" "in" "do"] @repeat
+;
+; ["for" "in" "do"] @repeat
 
 (import
   module: (identifier) @module)
@@ -74,129 +74,129 @@
 ; Tokens
 
 [
-  "!"
-  "$"
-  "%"
-  "&&"
-  "*"
-  "*>"
-  "+"
-  "++"
-  "-"
-  "/"
-  "::"
-  ":="
-  "<"
-  "<$>"
-  "<*"
-  "<*>"
-  "<="
-  "<|"
-  "<|>"
-  "="
-  "=="
-  "=>"
-  ">"
-  ">"
-  ">="
-  ">>"
-  ">>="
-  "@"
-  "^"
-  "|>"
-  "|>."
-  "||"
-  "←"
+;   "!"
+;   "$"
+;   "%"
+;   "&&"
+;   "*"
+;   "*>"
+;   "+"
+;   "++"
+;   "-"
+;   "/"
+;   "::"
+;   ":="
+;   "<"
+;   "<$>"
+;   "<*"
+;   "<*>"
+;   "<="
+;   "<|"
+;   "<|>"
+;   "="
+;   "=="
+;   "=>"
+;   ">"
+;   ">"
+;   ">="
+;   ">>"
+;   ">>="
+;   "@"
+;   "^"
+;   "|>"
+;   "|>."
+;   "||"
+;   "←"
   "→"
-  "↔"
-  "∘"
-  "∧"
-  "∨"
-  "≠"
-  "≤"
-  "≥"
+;   "↔"
+;   "∘"
+;   "∧"
+;   "∨"
+;   "≠"
+;   "≤"
+;   "≥"
 ] @operator
+;
+; [
+;   "@&"
+; ] @operator
 
 [
-  "@&"
-] @operator
-
-[
-  "attribute"
-  "by"
-  "end"
+  ; "attribute"
+  ; "by"
+  ; "end"
   "export"
-  "extends"
-  "fun"
-  "let"
-  "have"
-  "match"
+  ; "extends"
+  ; "fun"
+  ; "let"
+  ; "have"
+  ; "match"
   "open"
-  "return"
-  "universe"
+  ; "return"
+  ; "universe"
   "variable"
-  "where"
-  "with"
-  "λ"
-  (hash_command)
+  ; "where"
+  ; "with"
+  ; "λ"
+  (interactive)
   (prelude)
   (sorry)
-] @keyword
-
-[
-  "prefix"
-  "infix"
-  "infixl"
-  "infixr"
-  "postfix"
-  "notation"
-  "macro"
-  "macro_rules"
-  "syntax"
-  "elab"
-  "builtin_initialize"
-] @keyword
-
-[
-  "noncomputable"
-  "partial"
-  "private"
-  "protected"
-  "unsafe"
-] @keyword.modifier
-
-[
-  "apply"
-  "exact"
-  "rewrite"
-  "rw"
-  "simp"
-  (trivial)
 ] @keyword
 
 [
  "import"
 ] @keyword.import
 
-[
-  "catch"
-  "finally"
-  "try"
-] @exception
+; [
+;   "prefix"
+;   "infix"
+;   "infixl"
+;   "infixr"
+;   "postfix"
+;   "notation"
+;   "macro"
+;   "macro_rules"
+;   "syntax"
+;   "elab"
+;   "builtin_initialize"
+; ] @keyword
+
+; [
+;   "noncomputable"
+;   "partial"
+;   "private"
+;   "protected"
+;   "unsafe"
+; ] @keyword
+;
+; [
+;   "apply"
+;   "exact"
+;   "rewrite"
+;   "rw"
+;   "simp"
+;   (trivial)
+; ] @keyword
+
+; [
+;   "catch"
+;   "finally"
+;   "try"
+; ] @exception
 
 ((apply
-  name: (identifier) @exception)
+  function: (identifier) @exception)
  (#match? @exception "throw"))
 
-[
-  "unless"
-  "mut"
-] @keyword
-
-[(true) (false)] @boolean
+; [
+;   "unless"
+;   "mut"
+; ] @keyword
+;
+; [(true) (false)] @boolean
 
 (number) @number
-(float) @number.float
+; (float) @number.float
 
 (comment) @comment
 (char) @character
@@ -212,14 +212,14 @@
   "}" @punctuation.special)
 
 [
-  "(" ")"
-  "[" "]"
-  "{" "}"
-  "⟨" "⟩"
-] @punctuation.bracket
-
-["|" "," "." ":" ";"] @punctuation.delimiter
-
+ "(" ")"
+ "[" "]"
+ "{" "}"
+;  "⟨" "⟩"
+ ] @punctuation.bracket
+;
+; ["|" "," "." ":" ";"] @punctuation.delimiter
+;
 (sorry) @error
 
 ;; Error
