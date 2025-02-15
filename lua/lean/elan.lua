61018a8f3d5fff1669908c4172b89e123621f005
Julian Berman <Julian@GrayVines.com>
2025-02-15 12:51:11 -0500

    Typing for elan's state, and a toolchain list function.

--- lua/lean/elan.lua
+++ lua/lean/elan.lua
@@ -1,8 +1,40 @@
 local subprocess_check_output = require('lean._util').subprocess_check_output
-local elan = {}
+local elan = { toolchain = {} }
+
+---Elan's dumped state, which can partially change from project to project.
+---@class ElanState
+---@field elan_version ElanVersionInfo
+---@field toolchains ElanToolchainInfo
+
+---@class ElanVersionInfo
+---@field current string elan's own version
+---@field newest table
+
+---Information about installed and active Lean toolchains.
+---@class ElanToolchainInfo
+---@field active_override? table information about an overridden toolchain for the current project
+---@field default? table information about the default toolchain
+---@field installed ElanToolchain[] the currently installed toolchains
+---@field resolved_active table information about the resolved active toolchain
+
+---A single toolchain.
+---@class ElanToolchain
+---@field path string the path to the toolchain on this machine
+---@field resolved_name string the identifier for this toolchain
+
+--List the installed toolchains.
+function elan.toolchain.list()
+  local state = elan.state()
+  return vim
+    .iter(state.toolchains.installed)
+    :map(function(each)
+      return each.resolved_name
+    end)
+    :totable()
+end
 
 ---Dump elan's state.
----@return table
+---@return ElanState
 function elan.state()
   local stdout = subprocess_check_output { 'elan', 'dump-state' }
   return vim.json.decode(stdout)
