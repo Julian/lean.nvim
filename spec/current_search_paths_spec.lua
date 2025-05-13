74710ac6a76cc0133c281d85616b887be2dcec08
Julian Berman <Julian@GrayVines.com>
2025-05-13 17:31:56 -0400

    Minor test style.

--- spec/current_search_paths_spec.lua
+++ spec/current_search_paths_spec.lua
@@ -1,7 +1,9 @@
 local helpers = require 'spec.helpers'
 local project = require('spec.fixtures').project
 
-require('lean').setup {}
+local lean = require 'lean'
+
+lean.setup {}
 
 describe('lean.current_search_paths', function()
   for kind, path in project:files() do
@@ -9,7 +11,7 @@ describe('lean.current_search_paths', function()
       vim.cmd.edit(path)
       helpers.wait_for_ready_lsp()
 
-      local paths = require('lean').current_search_paths()
+      local paths = lean.current_search_paths()
       -- Sigh. We depend on import graph for another test, so now we can't
       -- really say exactly how many paths should appear here. I guess that's
       -- not too big of a loss, so eventually we can just delete this
@@ -29,7 +31,7 @@ describe('lean.current_search_paths', function()
     vim.cmd.edit 'someLoneLeanFile.lean'
     helpers.wait_for_ready_lsp()
 
-    local paths = require('lean').current_search_paths()
+    local paths = lean.current_search_paths()
     -- Sigh. We depend on import graph for another test, so now we can't
     -- really say exactly how many paths should appear here. I guess that's
     -- not too big of a loss, so eventually we can just delete this
