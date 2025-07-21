2d6caf7ef400a289573c24893745723c59e60ca0
Julian Berman <Julian@GrayVines.com>
2025-07-21 11:36:39 -0400

    Add Buffer.{create, is_valid, delete, force_delete}.
    Essentially allows moving create_buf over next.

--- lua/std/nvim/buffer.lua
+++ lua/std/nvim/buffer.lua
@@ -12,14 +12,60 @@ function Buffer:from_bufnr(bufnr)
 end
 
 ---Bind to the current buffer.
+---@return Buffer
 function Buffer:current()
   return self:from_bufnr(vim.api.nvim_get_current_buf())
+end
+
+---@class CreateBufferOpts
+---@field name? string the name of the new buffer
+---@field options? table<string, any> a table of buffer options
+---@field listed? boolean see :h nvim_create_buf (default true)
+---@field scratch? boolean see :h nvim_create_buf (default false)
+
+---Create a new buffer.
+---@param opts? CreateBufferOpts options for the new buffer
+---@return Buffer
+function Buffer.create(opts)
+  opts = opts or {}
+  local listed = opts.listed
+  if listed == nil then
+    listed = true
+  end
+  local scratch = opts.scratch
+  if scratch == nil then
+    scratch = false
+  end
+  local bufnr = vim.api.nvim_create_buf(listed, scratch)
+  for option, value in pairs(opts.options or {}) do
+    vim.bo[bufnr][option] = value
+  end
+  if opts.name ~= nil then
+    vim.api.nvim_buf_set_name(bufnr, opts.name)
+  end
+  return Buffer:from_bufnr(bufnr)
 end
 
 ---The buffer's name.
 ---@return string name
 function Buffer:name()
   return vim.api.nvim_buf_get_name(self.bufnr)
+end
+
+---Check if the buffer is valid.
+---@return boolean
+function Buffer:is_valid()
+  return vim.api.nvim_buf_is_valid(self.bufnr)
+end
+
+---Delete the buffer.
+function Buffer:delete()
+  vim.api.nvim_buf_delete(self.bufnr, {})
+end
+
+---Forcibly delete the buffer.
+function Buffer:force_delete()
+  vim.api.nvim_buf_delete(self.bufnr, { force = true })
 end
 
 return Buffer
--- spec/std/nvim/buffer_spec.lua
+++ spec/std/nvim/buffer_spec.lua
@@ -1,4 +1,5 @@
 local Buffer = require 'std.nvim.buffer'
+local Window = require 'std.nvim.window'
 
 describe('Buffer', function()
   describe('current', function()
@@ -10,6 +11,32 @@ describe('Buffer', function()
   describe('from_bufnr', function()
     it('defaults to current buffer', function()
       assert.are.same(Buffer:current(), Buffer:from_bufnr())
+    end)
+  end)
+
+  describe('create', function()
+    it('creates a new buffer', function()
+      local before = vim.api.nvim_list_bufs()
+      local buffer = Buffer.create {}
+      table.insert(before, buffer.bufnr)
+      assert.is.same(before, vim.api.nvim_list_bufs())
+      assert.is_true(vim.bo[buffer.bufnr].buflisted)
+    end)
+
+    it('can set the new buffer name', function()
+      local buffer = Buffer.create { name = '/tmp/test/buf' }
+      assert.is.same('/tmp/test/buf', buffer:name())
+    end)
+
+    it('can set listed and scratch', function()
+      local buffer = Buffer.create { listed = false, scratch = true }
+      assert.is_false(vim.bo[buffer.bufnr].buflisted)
+      assert.is.same('nofile', vim.bo[buffer.bufnr].buftype)
+    end)
+
+    it('can set additional options', function()
+      local buffer = Buffer.create { options = { buftype = 'prompt' } }
+      assert.is.same('prompt', vim.bo[buffer.bufnr].buftype)
     end)
   end)
 
@@ -30,6 +57,33 @@ describe('Buffer', function()
 
     it('is the current bufnr for the current buffer', function()
       assert.are.equal(vim.api.nvim_get_current_buf(), Buffer:current().bufnr)
+    end)
+  end)
+
+  describe('force_delete', function()
+    it('forcibly deletes the buffer', function()
+      local buffer = Buffer.create {}
+      vim.api.nvim_buf_set_lines(buffer.bufnr, 0, -1, false, { 'unsaved' })
+      local window = Window:split { buffer = buffer }
+
+      -- this still spews nonsense to stderr in the test run
+      assert.has_error(function()
+        buffer:delete()
+      end, 'Failed to unload buffer.')
+
+      assert.is_true(buffer:is_valid())
+      buffer:force_delete()
+      assert.is_false(buffer:is_valid())
+      assert.is_false(window:is_valid()) -- because its buffer is gone
+    end)
+  end)
+
+  describe('is_valid', function()
+    it('returns true for valid buffers and false for invalid ones', function()
+      local buffer = Buffer.create {}
+      assert.is_true(buffer:is_valid())
+      buffer:delete()
+      assert.is_false(buffer:is_valid())
     end)
   end)
 end)
