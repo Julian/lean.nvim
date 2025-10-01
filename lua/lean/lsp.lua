686565cca437035c5840d40fc80a495d3fe19786
Beau McCartney <mccartney.beausl@gmail.com>
2025-10-01 13:20:00 -0600

    remove deprecated lspconfig call

--- lua/lean/lsp.lua
+++ lua/lean/lsp.lua
@@ -225,7 +225,7 @@ function lsp.enable(opts)
         or 'Goals accomplished 🎉'
     end,
   })
-  require('lspconfig').leanls.setup(opts)
+  vim.lsp.config("leanls", opts)
 end
 
 ---Restart the Lean server for an open Lean 4 file.
