1f52f48ce1b4197fa2ef95c988bf44961ba42582
Scott Schwartz <scott7z123@gmail.com>
2026-03-04 01:24:52 -0800

    In leanls.lua, instantiate the config object after setup has run.
    The original code instantiates and stores the config at require-time,
    which is before setup has run, and it is not fully initialized. So, for
    example, setting the goal_markers.accomplished symbol in setup might not
    work.
    
    The config function has to be invoked later; elsewhere it is called near
    the point of use, so do that here too.
    
    A new test demonstrates the two cases.

--- spec/config_spec.lua
+++ spec/config_spec.lua
@@ -1,4 +1,5 @@
 local config = require 'lean.config'
+local saved_config = require 'lean.config'()
 
 describe('config', function()
   before_each(function()
@@ -12,5 +13,18 @@ describe('config', function()
   it('merges user config when set', function()
     vim.g.lean_config = { mappings = true }
     assert.are.same(config().mappings, true)
+  end)
+
+  it('demonstrate proper instantiation of lean.config', function()
+    require('lean').setup { goal_markers = { accomplished = "✓", }, }
+
+    -- after setup(), vim.g.lean_config has been updated with opts.
+    assert.are.same(vim.g.lean_config.goal_markers.accomplished, '✓')
+    -- similarly, evaluating the lean.config function returns vim.g.lean_config merged with defaults.
+    assert.are.same(config().goal_markers.accomplished, '✓')
+
+    -- however, evaluating config() at file-local require-time is too early;
+    -- it copies the value of vim.g.lean_config before it can be fully set up.
+    assert.are.same(saved_config.goal_markers.accomplished, '🎉')
   end)
 end)
