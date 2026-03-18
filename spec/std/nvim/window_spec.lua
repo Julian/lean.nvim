1ec26da9bf93023fbeff27532366aa17c6f042ed
Scott Schwartz <scott7z123@gmail.com>
2026-03-18 11:55:28 -0700

    Follow the existing pattern to handle the style field.

--- spec/std/nvim/window_spec.lua
+++ spec/std/nvim/window_spec.lua
@@ -102,8 +102,6 @@ describe('Window', function()
         col = 2,
       }
       local config = float:config()
-      -- nvim after 0.11.6 explicitly includes style, so elide it for this test
-      config.style = config.style ~= '' and config.style or nil
       assert.are.same({
         width = 15,
         height = 2,
@@ -117,6 +115,7 @@ describe('Window', function()
         border = config.border,
         focusable = config.focusable,
         mouse = config.mouse,
+        style = config.style,
         zindex = config.zindex,
       }, config)
       float:close()
