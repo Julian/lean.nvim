0603b974dbc8615499e4c7270d6598c1edc0c63d
Julian Berman <Julian@GrayVines.com>
2026-04-04 15:07:52 -0400

    Replace nui.popup with std Buffer/Window in demo overlay

--- demos/basic.lua
+++ demos/basic.lua
@@ -1,36 +1,33 @@
-local Popup = require 'nui.popup'
+local Buffer = require 'std.nvim.buffer'
+local Window = require 'std.nvim.window'
 
 -- In case recording crashes.
 vim.o.directory = ''
 vim.o.shada = ''
 
-DEMO = {
-  overlay_opts = {
-    position = '50%',
-    size = { width = 70, height = 10 },
-    enter = true,
-    focusable = true,
-    zindex = 50,
-    relative = 'editor',
-    border = {
-      padding = { top = 2, bottom = 2, left = 3, right = 3 },
-      style = 'rounded',
-    },
-    buf_options = { modifiable = true, readonly = false },
-    win_options = {
-      wrap = true,
-      winblend = 10,
-      winhighlight = 'Normal:Normal,FloatBorder:FloatBorder',
-    },
-  },
-}
+DEMO = {}
 
 ---Show an overlay ready for a popup message.
 ---
 ---Leaves actually typing and then exiting to be driven within VHS.
 function DEMO.popup()
-  Popup(DEMO.overlay_opts):mount()
-  vim.bo.filetype = 'markdown'
-  vim.b.completion = false -- disable blink, completion popping up is noisy
+  local width = 70
+  local height = 10
+  local buffer = Buffer.create{ listed = false, scratch = true }
+  local win = Window:from_id(vim.api.nvim_open_win(buffer.bufnr, true, {
+    relative = 'editor',
+    width = width,
+    height = height,
+    col = math.floor((vim.o.columns - width) / 2),
+    row = math.floor((vim.o.lines - height) / 2),
+    zindex = 50,
+    style = 'minimal',
+    border = 'rounded',
+  }))
+  win.o.wrap = true
+  win.o.winblend = 10
+  win.o.winhighlight = 'Normal:Normal,FloatBorder:FloatBorder'
+  buffer.o.filetype = 'markdown'
+  buffer.b.completion = false -- disable blink, completion popping up is noisy
   vim.cmd.startinsert()
 end
