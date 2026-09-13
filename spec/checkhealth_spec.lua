303bfe3027554a0d836430c8388e67829d8c68d5
Julian Berman <Julian@GrayVines.com>
2026-09-13 12:23:14 -0400

    Wait for health checks to finish asynchronously on 0.13+.

--- spec/checkhealth_spec.lua
+++ spec/checkhealth_spec.lua
@@ -5,6 +5,14 @@ local dedent = require('std.text').dedent
 describe('checkhealth', function()
   it('passes the health check', function()
     vim.cmd.checkhealth 'lean'
+
+    -- Healthchecks run asynchronously on Neovim 0.13+, which marks the buffer
+    -- nomodifiable once they're done.
+    local succeeded = vim.wait(10000, function()
+      return not vim.bo.modifiable
+    end)
+    assert.message('checkhealth never finished').is_true(succeeded)
+
     assert.has_match(
       dedent [[
         .*lean.nvim.*
